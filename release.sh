535eb01bf876ce93210ee006e8f9a41b7fa65cda
Edson Tirelli <ed.tirelli@gmail.com>
2008-06-20 13:34:19 +0000

    Adding release scripts to the repository
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20653 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ release.sh
@@ -0,0 +1,26 @@
+#/bin/sh
+
+echo "*************************************************************"
+echo "------> Importing release configuration"
+. release.env
+echo "CURRENT_VERSION = $CURRENT_VERSION"
+echo "RELEASE_VERSION = $RELEASE_VERSION"
+echo "NEW_VERSION     = $NEW_VERSION"
+echo "TAG_NAME        = $TAG_NAME"
+echo
+
+echo "*************************************************************"
+echo "------> Updating release version in configuration files"
+echo $ANT -f update-version.xml -Dcurrent="$CURRENT_VERSION" -Dnew="$RELEASE_VERSION" updateVersion
+echo
+
+echo "*************************************************************"
+echo "------> Commiting new version into trunk"
+echo $SVN commit -m "$JIRA_TICKET : preparing release. Updating files from version $CURRENT_VERSION to $RELEASE_VERSION"
+echo
+
+echo "*************************************************************"
+echo "------> Preparing the release"
+echo $MVN --batch-mode release:prepare -DdryRun=true
+
+ 
