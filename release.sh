f9f80302da51be1cbc2fc5c40745228b297b24a3
Edson Tirelli <ed.tirelli@gmail.com>
2008-06-20 15:48:17 +0000

    release script
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20664 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- release.sh
+++ release.sh
@@ -27,36 +27,36 @@ echo
 echo "*************************************************************"
 echo "------> Updating release version in configuration files"
 echo $ANT -f update-version.xml -Dcurrent="$CURRENT_VERSION" -Dnew="$RELEASE_VERSION" updateVersion
-echo
 if [ $? -ne 0 ] 
 then
     show_error "****** Error updating version numbers. Exiting. ******"
 fi
+echo
 
 echo "*************************************************************"
 echo "------> Commiting new version into trunk"
 echo $SVN commit -m "$JIRA_TICKET : preparing release. Updating files from version $CURRENT_VERSION to $RELEASE_VERSION"
-echo
 if [ $? -ne 0 ] 
 then
     show_error "****** Error commiting update files to trunk. Exiting. ******"
 fi
+echo
 
 echo "*************************************************************"
 echo "------> Preparing the release"
 echo $MVN --batch-mode release:clean release:prepare 
-echo
 if [ $? -ne 0 ] 
 then
     show_error "****** Error preparing the release. Exiting. ******"
 fi
+echo
 
 echo "*************************************************************"
 echo "------> Generating artifacts"
 echo $MVN -Ddocumentation -Declipse -Dmaven.test.skip -Dydoc.home=$YDOC_HOME package javadoc:javadoc assembly:assembly
-echo
 if [ $? -ne 0 ] 
 then
     show_error "****** Error generating distribution artifacts. Exiting. ******"
 fi
+echo
 
