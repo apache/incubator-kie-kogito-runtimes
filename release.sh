664a0f33d8d402da09287725533d94566ab06883
Edson Tirelli <ed.tirelli@gmail.com>
2008-06-20 16:29:34 +0000

    working on the release script
    git-svn-id: https://svn.jboss.org/repos/labs/labs/jbossrules/trunk@20666 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- release.sh
+++ release.sh
@@ -1,15 +1,66 @@
 #/bin/sh
 
 #
-#  Shows the error message and stops the script execution
+#  Checks if there is an error flag active and if so,
+#  shows an error message and terminates the script
 #
 #  $1 : error message to show
 #
-show_error() {
-    echo 
-    echo "ERROR: $1"
-    echo
-    exit 1
+check_error() {
+    if [ $? -ne 0 ] 
+    then
+        echo 
+        echo "ERROR: $1"
+        echo
+        exit 1
+    fi
+}
+
+#
+#  Generate the release file
+#
+#  $1 : Release version
+#  $2 : Development version
+#  $3 : Release Tag
+#
+create_release_file() {
+    REL_VERSION=$1
+    DEV_VERSION=$2
+    TAG_VERSION=$3
+    
+    cat > release.properties <<EOF 
+#release configuration
+#Fri Jun 20 12:51:50 BRT 2008
+scm.tag=$TAG_VERSION
+project.rel.org.drools\:drools-ant=$REL_VERSION
+project.dev.org.drools\:drools-templates=$DEV_VERSION
+project.dev.org.drools\:drools-clips=$DEV_VERSION
+project.dev.org.drools.solver\:drools-solver=$DEV_VERSION
+project.dev.org.drools\:drools-repository=$DEV_VERSION
+project.rel.org.drools\:drools-repository=$REL_VERSION
+project.rel.org.drools.solver\:drools-solver-core=$REL_VERSION
+project.rel.org.drools\:drools-clips=$REL_VERSION
+project.rel.org.drools.solver\:drools-solver=$REL_VERSION
+project.dev.org.drools\:drools-decisiontables=$DEV_VERSION
+project.dev.org.drools.solver\:drools-solver-examples=$DEV_VERSION
+project.dev.org.drools\:drools-compiler=$DEV_VERSION
+project.rel.org.drools\:drools-core=$REL_VERSION
+project.dev.org.drools\:drools-verifier=$DEV_VERSION
+project.dev.org.drools\:drools-guvnor=$DEV_VERSION
+project.rel.org.drools\:drools-compiler=$REL_VERSION
+project.dev.org.drools\:drools-ant=$DEV_VERSION
+project.rel.org.drools\:drools-verifier=$REL_VERSION
+project.rel.org.drools\:drools-decisiontables=$REL_VERSION
+project.rel.org.drools\:drools-templates=$REL_VERSION
+project.dev.org.drools\:drools-jsr94=$DEV_VERSION
+project.rel.org.drools\:drools-guvnor=$REL_VERSION
+project.dev.org.drools.solver\:drools-solver-core=$DEV_VERSION
+project.rel.org.drools\:drools=$REL_VERSION
+project.rel.org.drools\:drools-jsr94=$REL_VERSION
+project.dev.org.drools\:drools=$DEV_VERSION
+project.dev.org.drools\:drools-core=$DEV_VERSION
+project.rel.org.drools.solver\:drools-solver-examples=$REL_VERSION
+EOF
 }
 
 #
@@ -27,36 +78,27 @@ echo
 echo "*************************************************************"
 echo "------> Updating release version in configuration files"
 echo $ANT -f update-version.xml -Dcurrent="$CURRENT_VERSION" -Dnew="$RELEASE_VERSION" updateVersion
-if [ $? -ne 0 ] 
-then
-    show_error "****** Error updating version numbers. Exiting. ******"
-fi
+check_error "****** Error updating version numbers. Exiting. ******"
 echo
 
 echo "*************************************************************"
 echo "------> Commiting new version into trunk"
 echo $SVN commit -m "$JIRA_TICKET : preparing release. Updating files from version $CURRENT_VERSION to $RELEASE_VERSION"
-if [ $? -ne 0 ] 
-then
-    show_error "****** Error commiting update files to trunk. Exiting. ******"
-fi
+check_error "****** Error commiting update files to trunk. Exiting. ******"
 echo
 
 echo "*************************************************************"
 echo "------> Preparing the release"
-echo $MVN --batch-mode release:clean release:prepare 
-if [ $? -ne 0 ] 
-then
-    show_error "****** Error preparing the release. Exiting. ******"
-fi
+echo $MVN release:clean
+check_error "****** Error cleaning up for the release. Exiting. ******"
+create_release_file $RELEASE_VERSION $NEW_VERSION $TAG_NAME
+echo $MVN --batch-mode release:prepare 
+check_error "****** Error preparing the release. Exiting. ******"
 echo
 
 echo "*************************************************************"
 echo "------> Generating artifacts"
-echo $MVN -Ddocumentation -Declipse -Dmaven.test.skip -Dydoc.home=$YDOC_HOME package javadoc:javadoc assembly:assembly
-if [ $? -ne 0 ] 
-then
-    show_error "****** Error generating distribution artifacts. Exiting. ******"
-fi
+echo $MVN -Ddocumentation -Declipse -Dmaven.test.skip -Dydoc.home=$YDOC_HOME -DlocalEclipseDrop=/home/hudson/configs/jboss-rules/local-eclipse-drop-mirror package javadoc:javadoc assembly:assembly
+check_error "****** Error generating distribution artifacts. Exiting. ******"
 echo
-
+ 
