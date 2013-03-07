3415c842303185653c69bfca8c716cd5a3a8e198
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2013-03-07 14:39:24 +0100

    detectDuplicateClassesOverModules

--- script/checks/detectSplitPackagesOverModules.sh
+++ script/checks/detectSplitPackagesOverModules.sh
@@ -11,10 +11,10 @@ projectDir=${scriptDir}/../../..
 
 echo "Split packages in src/main/java:"
 echo "================================"
-ls -R ${projectDir} | grep "src/main/java/" | sed "s/\(.*\/src\/main\/java\/\)\(.*\)\:/\2/g" | sort | uniq -c | grep -v "\s*1 "
+ls -R ${projectDir} | grep "src/main/java/" | grep -v "target" | sed "s/\(.*\/src\/main\/java\/\)\(.*\)\:/\2/g" | sort | uniq -c | grep -v "\s*1 "
 echo ""
 
 echo "Split packages in src/test/java:"
 echo "================================"
-ls -R ${projectDir} | grep "src/test/java/" | sed "s/\(.*\/src\/test\/java\/\)\(.*\)\:/\2/g" | sort | uniq -c | grep -v "\s*1 "
+ls -R ${projectDir} | grep "src/test/java/" | grep -v "target" | sed "s/\(.*\/src\/test\/java\/\)\(.*\)\:/\2/g" | sort | uniq -c | grep -v "\s*1 "
 echo ""
