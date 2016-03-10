fb52da0aba0fe0feb10a96de3d41301ee728ed91
kiereleaseuser <kiereleaseuser@gmail.com>
2016-03-10 18:32:44 +0100

    create_filemgmt_links.sh: copy also jBPM artifacts

--- script/release/create_filemgmt_links.sh
+++ script/release/create_filemgmt_links.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Update the version for for all droolsjbpm repositories
+# Update the symbolic links lastest and latestFinal in 
+# drools@filemgmt.jboss.org:downloads_htdocs/drools/release/
+# drools@filemgmt.jboss.org:downloads_htdocs/docs_htdocs/drools/release/
+# optaplanner@filemgmt.jboss.org:downloads_htdocs/optaplanner/release/
+# optaplanner@filemgmt.jboss.org:downloads_htdocs/docs_htdocs/optaplanner/release/
+# jbpm@filemgmt.jboss.org:downloads_htdocs/jbpm/release/
+# jbpm@filemgmt.jboss.org:downloads_htdocs/docs_htdocs/jbpm/release/
 
 initializeWorkingDirAndScriptDir() {
     # Set working directory and remove all symbolic links
@@ -22,118 +28,105 @@ initializeWorkingDirAndScriptDir() {
 initializeWorkingDirAndScriptDir
 droolsjbpmOrganizationDir="$scriptDir/../../.."
 
-# TODO move jbpm from sourceforge to filemgmt
-withoutJbpm=true
-if [ $# != 1 ] ; then
-    echo "ERROR jbpm is not supported yet!"
-    exit 1
-fi
 
 if [ $# != 1 ] && [ $# != 2 ] ; then
     echo
     echo "Usage:"
-    echo "  $0 drools and jbpm version"
+    echo "  $0 kie version"
     echo "For example:"
     echo "  $0 6.1.0.Final"
     echo
     exit 1
 fi
-droolsVersion=$1
-echo "The drools, guvnor, ... jbpm version: ${droolsVersion}"
-if [ "$withoutJbpm" != 'true' ]; then
-    jbpmVersion=$2
-    echo "The jbpm version: $jbpmVersion"
-fi
+
+kieVersion=$1
+
+echo "The kie-version: ${kieVersion}"
 echo -n "Is this ok? (Hit control-c if is not): "
 read ok
 
-cd $droolsjbpmOrganizationDir/droolsjbpm-build-distribution/droolsjbpm-uber-distribution
-cd target
+cd $droolsjbpmOrganizationDir
 mkdir filemgmt_links
 cd filemgmt_links
 
-urlBase="drools@filemgmt.jboss.org:"
+urlBase_drools="drools@filemgmt.jboss.org:"
 
 ###############################################################################
-# latest links
+# latest drools links
 ###############################################################################
-touch ${droolsVersion}
+touch ${kieVersion}
 rm latest
-ln -s ${droolsVersion} latest
+ln -s ${kieVersion} latest
 
 echo "Uploading normal links..."
-rsync -a --protocol=28 latest $urlBase/downloads_htdocs/drools/release/
-rsync -a --protocol=28 latest $urlBase/docs_htdocs/drools/release/
-
-echo "<meta http-equiv=\"refresh\" content=\"0;url=latest/drools-distribution-${droolsVersion}.zip\">" > drools-latest.html
-echo "<meta http-equiv=\"refresh\" content=\"0;url=latest/droolsjbpm-integration-distribution-${droolsVersion}.zip\">" > droolsjbpm-integration-latest.html
-echo "<meta http-equiv=\"refresh\" content=\"0;url=latest/kie-drools-wb-distribution-${droolsVersion}.zip\">" > kie-drools-wb-distribution-latest.html
-echo "<meta http-equiv=\"refresh\" content=\"0;url=latest/droolsjbpm-tools-distribution-${droolsVersion}.zip\">" > droolsjbpm-tools-latest.html
-rsync -a --protocol=28  *-latest.html $urlBase/downloads_htdocs/drools/release/
+rsync -a --protocol=28 latest $urlBase_drools/downloads_htdocs/drools/release/
+rsync -a --protocol=28 latest $urlBase_drools/docs_htdocs/drools/release/
 
 ###############################################################################
-# latestFinal links
+# latestFinal drools links
 ###############################################################################
-if [[ "${droolsVersion}" == *Final* ]]; then
+if [[ "${kieVersion}" == *Final* ]]; then
     rm latestFinal
-    ln -s ${droolsVersion} latestFinal
+    ln -s ${kieVersion} latestFinal
     echo "Uploading Final links..."
-    rsync -a --protocol=28  latestFinal $urlBase/downloads_htdocs/drools/release/
-    rsync -a --protocol=28  latestFinal $urlBase/docs_htdocs/drools/release/
-
-    echo "<meta http-equiv=\"refresh\" content=\"0;url=latestFinal/drools-distribution-${droolsVersion}.zip\">" > drools-latestFinal.html
-    echo "<meta http-equiv=\"refresh\" content=\"0;url=latestFinal/droolsjbpm-integration-distribution-${droolsVersion}.zip\">" > droolsjbpm-integration-latestFinal.html
-    echo "<meta http-equiv=\"refresh\" content=\"0;url=latestFinal/kie-drools-wb-distribution-${droolsVersion}.zip\">" > kie-drools-wb-distribution-latest.html
-    echo "<meta http-equiv=\"refresh\" content=\"0;url=latestFinal/droolsjbpm-tools-distribution-${droolsVersion}.zip\">" > droolsjbpm-tools-latestFinal.html
-    rsync -a --protocol=28  *-latestFinal.html $urlBase/downloads_htdocs/drools/release/
+    rsync -a --protocol=28  latestFinal $urlBase_drools/downloads_htdocs/drools/release/
+    rsync -a --protocol=28  latestFinal $urlBase_drools/docs_htdocs/drools/release/
 fi
-
-###############################################################################
-# empty target directory
-###############################################################################
-cd ..
-rm -rf filemgmt_links
-mkdir filemgmt_links
-cd filemgmt_links
 
 urlBase_optaplanner="optaplanner@filemgmt.jboss.org:"
 
 ###############################################################################
-# latest links for optaplanner
+# latest optaplanner links
 ###############################################################################
-touch ${droolsVersion}
+touch ${kieVersion}
 rm latest
-ln -s ${droolsVersion} latest
+ln -s ${kieVersion} latest
 
 echo "Uploading normal links for optaplanner..."
 rsync -a --protocol=28 latest $urlBase_optaplanner/downloads_htdocs/optaplanner/release/
 rsync -a --protocol=28 latest $urlBase_optaplanner/docs_htdocs/optaplanner/release/
 
-echo "<meta http-equiv=\"refresh\" content=\"0;url=latest/optaplanner-distribution-${droolsVersion}.zip\">" > optaplanner-latest.html
-rsync -a --protocol=28  *-latest.html $urlBase_optaplanner/downloads_htdocs/optaplanner/release/
 
 ###############################################################################
-# latestFinal links for optaplanner
+# latestFinal optaplanner links
 ###############################################################################
 
-if [[ "${droolsVersion}" == *Final* ]]; then
+if [[ "${kieVersion}" == *Final* ]]; then
     rm latestFinal
-    ln -s ${droolsVersion} latestFinal
+    ln -s ${kieVersion} latestFinal
     echo "Uploading Final links for optaplanner..."
     rsync -a --protocol=28 latestFinal $urlBase_optaplanner/downloads_htdocs/optaplanner/release/
     rsync -a --protocol=28 latestFinal $urlBase_optaplanner/docs_htdocs/optaplanner/release/
-
-    echo "<meta http-equiv=\"refresh\" content=\"0;url=latest/optaplanner-distribution-${droolsVersion}.zip\">" > optaplanner-latestFinal.html
-    rsync -a --protocol=28  *-latestFinal.html $urlBase_optaplanner/downloads_htdocs/optaplanner/release/
 fi
 
+urlBase_jbpm="jbpm@filemgmt.jboss.org:"
 
 ###############################################################################
-# JBoss Tools links
+# latest jbpm links
 ###############################################################################
-# mkdir jbosstools
-# cd jbosstools
-# rm ${droolsVersion}
-# ln ../../../../../../drools/release/${droolsVersion}/org.drools.updatesite ${droolsVersion}
-# rsync -a --protocol=28 ${droolsVersion} $urlBase/downloads_htdocs/tools/updates/stable/juno/soa-tooling/droolsjbpm/
-# cd ..
+touch ${kieVersion}
+rm latest
+ln -s ${kieVersion} latest
+
+echo "Uploading normal links for jbpm..."
+rsync -a --protocol=28 latest $urlBase_jbpm/downloads_htdocs/jbpm/release/
+rsync -a --protocol=28 latest $urlBase_jbpm/docs_htdocs/jbpm/release/
+
+
+###############################################################################
+# latestFinal jbpm links
+###############################################################################
+
+if [[ "${kieVersion}" == *Final* ]]; then
+    rm latestFinal
+    ln -s ${kieVersion} latestFinal
+    echo "Uploading Final links for jbpm..."
+    rsync -a --protocol=28 latestFinal $urlBase_jbpm/downloads_htdocs/jbpm/release/
+    rsync -a --protocol=28 latestFinal $urlBase_jbpm/docs_htdocs/jbpm/release/
+fi
+
+###############################################################################
+# remove filemgmt_links directory
+###############################################################################
+cd ..
+rm -rf filemgmt_links
