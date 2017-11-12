081bf84253b74c78d294dee42ee5b9fcd009372c
Petr Široký <siroky.petr@gmail.com>
2017-11-12 13:45:55 +0100

    mvn-all: optional clean-up of build artifacts to save disk space
    The script now takes another script as parameter(--clean-up-script=<path>).
    That clean-up script will be executed for every repository in the list.

--- script/mvn-all.sh
+++ script/mvn-all.sh
@@ -19,6 +19,20 @@ initializeWorkingDirAndScriptDir() {
     # Set script directory and remove other symbolic links (parent directory links)
     scriptDir=`pwd -P`
 }
+
+printUsage() {
+    echo
+    echo "Usage:"
+    echo "  $0 <Maven arguments> [--repo-list=<list-of-repositories>] [--clean-up-script=<absolute-path>]"
+    echo "For example:"
+    echo "  $0 --version"
+    echo "  $0 -DskipTests clean install"
+    echo "  $0 -Dfull clean install"
+    echo "  $0 clean test --repo-list=drools,jbpm"
+    echo "  $0 clean test --clean-up-script=\`pwd\`/remove-big-dirs.sh"
+    echo
+}
+
 initializeWorkingDirAndScriptDir
 droolsjbpmOrganizationDir="$scriptDir/../.."
 
@@ -35,29 +49,46 @@ do
             REPOSITORY_LIST=${REPOSITORY_LIST//,/ }
         ;;
 
+        --clean-up-script=*)
+            CLEAN_UP_SCRIPT=$(echo $arg | sed 's/[-a-zA-Z0-9]*=//')
+        ;;
+
         *)
             MVN_ARG_LINE+=("$arg")
         ;;
     esac
 done
 
-if [ "x$MVN_ARG_LINE" = "x" ] ; then
-    echo
-    echo "Usage:"
-    echo "  $0 <arguments of maven> [--repo-list=<list-of-repositories>]"
-    echo "For example:"
-    echo "  $0 --version"
-    echo "  $0 -DskipTests clean install"
-    echo "  $0 -Dfull clean install"
-    echo "  $0 clean test --repo-list=drools,jbpm"
-    echo
+
+# check that Maven args are non empty
+if [ "$MVN_ARG_LINE" = "" ] ; then
+    echo "No Maven arguments specified!"
+    printUsage
     exit 1
 fi
 
-startDateTime=`date +%s`
+# check the clean-up script exists (if specified via arg)
+if [ "$CLEAN_UP_SCRIPT" != "" ] && [ ! -f "$CLEAN_UP_SCRIPT" ]; then
+    echo "Invalid clean-up script specified: $CLEAN_UP_SCRIPT. The file does not exist!"
+    printUsage
+    exit 2
+fi
+
+mvnBin="mvn"
+if [ -a $M3_HOME/bin/mvn ] ; then
+    mvnBin="$M3_HOME/bin/mvn"
+fi
+echo "Using Maven binary '$mvnBin'"
+"$mvnBin" -v
+echo
 
 echo "Maven arg. line=${MVN_ARG_LINE[@]}"
+if [ "$CLEAN_UP_SCRIPT" != "" ]; then
+    echo "Using clean-up script '$CLEAN_UP_SCRIPT'"
+fi
+
 cd "$droolsjbpmOrganizationDir"
+startDateTime=`date +%s`
 
 for repository in $REPOSITORY_LIST; do
     echo
@@ -71,17 +102,17 @@ for repository in $REPOSITORY_LIST; do
         echo "==============================================================================="
         cd $repository
 
-        if [ -a $M3_HOME/bin/mvn ] ; then
-            $M3_HOME/bin/mvn "${MVN_ARG_LINE[@]}"
-        else
-            mvn "${MVN_ARG_LINE[@]}"
-        fi
-
+        "$mvnBin" "${MVN_ARG_LINE[@]}"
         returnCode=$?
-        cd ..
+
         if [ $returnCode != 0 ] ; then
             exit $returnCode
         fi
+        if [ "$CLEAN_UP_SCRIPT" != "" ]; then
+            echo "Running clean-up script $CLEAN_UP_SCRIPT for repository $repository"
+            sh "$CLEAN_UP_SCRIPT" `pwd`
+        fi
+        cd ..
     fi
 done
 
