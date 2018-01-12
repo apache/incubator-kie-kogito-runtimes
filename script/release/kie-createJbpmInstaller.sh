53833dec92ab89fc03b5bfb386fb8312063a7f42
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2018-01-12 14:30:12 +0100

    fixed jbpm installer for internal reps (#665)

--- script/release/kie-createJbpmInstaller.sh
+++ script/release/kie-createJbpmInstaller.sh
@@ -2,19 +2,25 @@
 
 # this script build the jbpm-installer and jbpm-installer full
 
+if [ "target" == "Nexus" ]; then
+   URLgroup="public-jboss"
+else
+   URLgroup="kie-group"
+fi
+
 createJbpmInstaller(){
         # download and unzip the jbpm-installer-<version>.zip
-        wget https://origin-repository.jboss.org/nexus/content/groups/kie-group/org/jbpm/jbpm-installer/$version/jbpm-installer-$version.zip
+        wget https://repository.jboss.org/nexus/content/groups/$group/org/jbpm/jbpm-installer/$version/jbpm-installer-$version.zip
         unzip jbpm-installer*.zip
 
         # modifications in build.properties
         sed -i -e '/^#/!s/jBPM.version=\${snapshot.version}/jBPM.version=\${release.version}/g' jbpm-installer-$version/build.properties
-        sed -i -e '/^#/!s/jBPM.url=http.*/jBPM.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/public-jboss\/org\/drools\/droolsjbpm-bpms-distribution\/\${jBPM.version}\/droolsjbpm-bpms-distribution-$\{jBPM.version}-bin.zip/g' jbpm-installer-$version/build.properties
-        sed -i -e '/^#/!s/jBPM.console.url=http.*/jBPM.console.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/public-jboss\/org\/kie\/kie-wb\/\${jBPM.version}\/kie-wb-\${jBPM.version}-wildfly10.war/g' jbpm-installer-$version/build.properties
-        sed -i -e '/^#/!s/jBPM.casemgmt.url=https.*/jBPM.casemgmt.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/public-jboss\/org\/jbpm\/jbpm-wb-case-mgmt-showcase\/\${jBPM.version}\/jbpm-wb-case-mgmt-showcase-\${jBPM.version}-wildfly10.war/g' jbpm-installer-$version/build.properties
-        sed -i -e '/^#/!s/kie.server.url=http.*/kie.server.url=http:\/\/repository.jboss.org\/nexus\/content\/groups\/public-jboss\/org\/kie\/server\/kie-server\/${jBPM.version}\/kie-server-\${jBPM.version}-ee7.war/g' jbpm-installer-$version/build.properties
+        sed -i -e '/^#/!s/jBPM.url=http.*/jBPM.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/$URLgroup\/org\/drools\/droolsjbpm-bpms-distribution\/\${jBPM.version}\/droolsjbpm-bpms-distribution-$\{jBPM.version}-bin.zip/g' jbpm-installer-$version/build.properties
+        sed -i -e '/^#/!s/jBPM.console.url=http.*/jBPM.console.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/$URLgroup\/org\/kie\/kie-wb\/\${jBPM.version}\/kie-wb-\${jBPM.version}-wildfly10.war/g' jbpm-installer-$version/build.properties
+        sed -i -e '/^#/!s/jBPM.casemgmt.url=https.*/jBPM.casemgmt.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/$URLgroup\/org\/jbpm\/jbpm-wb-case-mgmt-showcase\/\${jBPM.version}\/jbpm-wb-case-mgmt-showcase-\${jBPM.version}-wildfly10.war/g' jbpm-installer-$version/build.properties
+        sed -i -e '/^#/!s/kie.server.url=http.*/kie.server.url=http:\/\/repository.jboss.org\/nexus\/content\/groups\/$URLgroup\/org\/kie\/server\/kie-server\/${jBPM.version}\/kie-server-\${jBPM.version}-ee7.war/g' jbpm-installer-$version/build.properties
         sed -i -e '/^#/!s/droolsjbpm.eclipse.version=\${snapshot.version}/droolsjbpm.eclipse.version=\${release.version}/g' jbpm-installer-$version/build.properties
-        sed -i -e '/^#/!s/droolsjbpm.eclipse.url=http.*/droolsjbpm.eclipse.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/public-jboss\/org\/drools\/org.drools.updatesite\/\${droolsjbpm.eclipse.version}\/org.drools.updatesite-\${droolsjbpm.eclipse.version}.zip/g' jbpm-installer-$version/build.properties
+        sed -i -e '/^#/!s/droolsjbpm.eclipse.url=http.*/droolsjbpm.eclipse.url=https:\/\/repository.jboss.org\/nexus\/content\/groups\/$URLgroup\/org\/drools\/org.drools.updatesite\/\${droolsjbpm.eclipse.version}\/org.drools.updatesite-\${droolsjbpm.eclipse.version}.zip/g' jbpm-installer-$version/build.properties
 
         # add modified build.properties to jbpm-installer-<version>.zip
         zip -ur jbpm-installer-$version.zip jbpm-installer-$version/build.properties
