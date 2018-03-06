2e39c290bb328f40ca8924b720a4cd4a39b29724
Michael Biarnés Kiefer <mbiarnes@redhat.com>
2018-03-06 16:14:02 +0100

    increased gwt compile memory (#716)

--- script/release/kie-deployLocally.sh
+++ script/release/kie-deployLocally.sh
@@ -8,7 +8,7 @@ if [ -d $MAVEN_REPO_LOCAL ]; then
     rm -rf $MAVEN_REPO_LOCAL/org/drools/
     rm -rf $MAVEN_REPO_LOCAL/org/jbpm/
     rm -rf $MAVEN_REPO_LOCAL/org/optaplanner/
-    rm -rf $MAVEN_REPO_LOCAL/org/guvnor/
+    rm -rf $MAVEN_REPO_LOCAL/org/uberfire/
     
 fi
 
@@ -41,12 +41,12 @@ if [ "$target" == "community" ]; then
    deployDir=$WORKSPACE/community-deploy-dir
    # (1) do a full build, but deploy only into local dir
    # we will deploy into remote staging repo only once the whole build passed (to save time and bandwith)   
-   ./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -Dfull -Drelease -T2 -DaltDeploymentRepository=local::default::file://$deployDir -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx4g -Xms1g -Xss1M" -Dgwt.compiler.localWorkers=2
+   ./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -Dfull -Drelease -T2 -DaltDeploymentRepository=local::default::file://$deployDir -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xms1g -Xmx10g -Xms1g -Xss1M" -Dgwt.compiler.localWorkers=2
   
 else
    deployDir=$WORKSPACE/prod-deploy-dir
    # (1) do a full build with prod look & feel (-Dproductized), but deploy only into local dir
    # we will deploy into remote staging repo only once the whole build passed (to save time and bandwith)   
-   ./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -Dfull -Dproductized -Drelease -T2 -DaltDeploymentRepository=local::default::file://$deployDir -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xmx4g -Xms1g -Xss1M" -Dgwt.compiler.localWorkers=2
+   ./droolsjbpm-build-bootstrap/script/mvn-all.sh -B -e -U clean deploy -Dfull -Dproductized -Drelease -T2 -DaltDeploymentRepository=local::default::file://$deployDir -Dmaven.test.failure.ignore=true -Dgwt.memory.settings="-Xms1g -Xmx10g -Xms1g -Xss1M" -Dgwt.compiler.localWorkers=2
 
 fi
