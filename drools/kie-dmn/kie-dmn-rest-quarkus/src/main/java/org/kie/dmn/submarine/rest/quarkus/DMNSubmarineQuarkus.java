c79d6bb4db3cfa844236f9e9ec2513e31c11a7f2
tarilabs <matteo.mortari@gmail.com>
2019-04-17 09:43:23 +0200

    append byName methods

--- drools/kie-dmn/kie-dmn-rest-quarkus/src/main/java/org/kie/dmn/submarine/rest/quarkus/DMNSubmarineQuarkus.java
+++ drools/kie-dmn/kie-dmn-rest-quarkus/src/main/java/org/kie/dmn/submarine/rest/quarkus/DMNSubmarineQuarkus.java
@@ -70,6 +70,19 @@ public class DMNSubmarineQuarkus {
         return new DMNRuntimeImpl(knowledgeBase);
     }
 
+    public static DMNModel modelByName(DMNRuntime dmnRuntime, String modelName) {
+        List<DMNModel> modelsWithName = dmnRuntime.getModels().stream().filter(m -> modelName.equals(m.getName())).collect(Collectors.toList());
+        if (modelsWithName.size() == 1) {
+            return modelsWithName.get(0);
+        } else {
+            throw new RuntimeException("Multiple model with the same name: " + modelName);
+        }
+    }
+
+    public static DMNResult evaluate(DMNRuntime dmnRuntime, String modelName, Map<String, Object> dmnContext) {
+        return evaluate(dmnRuntime, modelByName(dmnRuntime, modelName).getNamespace(), modelName, dmnContext);
+    }
+
     public static DMNResult evaluate(DMNRuntime dmnRuntime, String modelNamespace, String modelName, Map<String, Object> dmnContext) {
         DMNEvaluationResult evaluationResult = DMNEvaluationUtils.evaluate(dmnRuntime,
                                                                            modelNamespace,
