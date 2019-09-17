7efd5286d253ca55ebb1e973995314f9ea323439
Edoardo Vacchi <uncommonnonsense@gmail.com>
2019-09-17 17:43:26 +0200

    support 'unit:' prefix for backwards compat

--- jbpm/jbpm-flow/src/main/java/org/jbpm/workflow/core/node/RuleSetNode.java
+++ jbpm/jbpm-flow/src/main/java/org/jbpm/workflow/core/node/RuleSetNode.java
@@ -41,14 +41,24 @@ public class RuleSetNode extends StateBasedNode implements ContextContainer {
 
     public static abstract class RuleType implements Serializable {
 
+        public static final String UNIT_RULEFLOW_PREFIX = "unit:";
+
         public static RuleType of(String name, String language) {
             if (language.equals(DRL_LANG)) {
-                return ruleFlowGroup(name);
+                return parseRuleFlowGroup(name);
             } else if (language.equals(RULE_UNIT_LANG)){
                 return ruleUnit(name);
             } else {
                 throw new IllegalArgumentException("Unsupported language " + language);
             }
+        }
+
+        public static RuleType parseRuleFlowGroup(String name) {
+            if (name.startsWith("unit:")) {
+                String unitId = name.substring(UNIT_RULEFLOW_PREFIX.length());
+                return ruleUnit(unitId);
+            }
+            return ruleFlowGroup(name);
         }
 
         public static RuleFlowGroup ruleFlowGroup(String name) {
