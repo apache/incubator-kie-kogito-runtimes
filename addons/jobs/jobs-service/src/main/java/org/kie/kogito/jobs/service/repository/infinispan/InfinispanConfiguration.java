f8cfe51dbaf3704fea84c423050a48d40b5dc35f
Tiago Dolphine <tiagodolphine@gmail.com>
2020-01-23 15:46:35 -0300

    KOGITO-885 [Jobs Service] Add health check for Jobs Service

--- addons/jobs/jobs-service/src/main/java/org/kie/kogito/jobs/service/repository/infinispan/InfinispanConfiguration.java
+++ addons/jobs/jobs-service/src/main/java/org/kie/kogito/jobs/service/repository/infinispan/InfinispanConfiguration.java
@@ -17,19 +17,18 @@
 package org.kie.kogito.jobs.service.repository.infinispan;
 
 import java.util.Optional;
-import java.util.concurrent.CompletionStage;
 
 import javax.annotation.Priority;
-import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.interceptor.Interceptor;
 
-import io.quarkus.runtime.StartupEvent;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.eclipse.microprofile.health.Readiness;
 import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.kie.kogito.infinispan.health.InfinispanHealthCheck;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,7 +44,7 @@ public class InfinispanConfiguration {
      */
     public static class Caches {
 
-        private Caches(){
+        private Caches() {
 
         }
 
@@ -69,12 +68,14 @@ public class InfinispanConfiguration {
                 .map(p -> cacheManagerInstance.get());
     }
 
-    CompletionStage<Void> onStart(@Observes StartupEvent startupEvent) {
-        return ReactiveStreams.of(Caches.all())
-                .forEach(name -> cacheManager
-                        .map(RemoteCacheManager::administration)
-                        .ifPresent(adm -> adm.getOrCreateCache(name, (String) null)))
-                .run()
-                .thenAccept(c -> LOGGER.info("Executed Infinispan configuration"));
+    @Produces
+    @Readiness
+    public InfinispanHealthCheck infinispanHealthCheck(@ConfigProperty(name = PERSISTENCE_CONFIG_KEY)
+                                                               Optional<String> persistence,
+                                                       Instance<RemoteCacheManager> cacheManagerInstance) {
+        return persistence
+                .filter("infinispan"::equals)
+                .map(p -> new InfinispanHealthCheck(cacheManagerInstance))
+                .orElse(null);
     }
 }
