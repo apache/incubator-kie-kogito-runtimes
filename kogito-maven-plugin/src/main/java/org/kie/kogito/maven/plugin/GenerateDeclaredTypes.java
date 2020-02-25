45ea27da20f00093d5c4efa22fcec6e628950f95
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-02-25 16:11:37 +0100

    unused import

--- kogito-maven-plugin/src/main/java/org/kie/kogito/maven/plugin/GenerateDeclaredTypes.java
+++ kogito-maven-plugin/src/main/java/org/kie/kogito/maven/plugin/GenerateDeclaredTypes.java
@@ -2,7 +2,6 @@ package org.kie.kogito.maven.plugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
