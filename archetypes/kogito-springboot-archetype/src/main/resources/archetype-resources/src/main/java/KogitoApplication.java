38db01d5e9899a7b61f4078b8a102ed3020d2ae3
Christopher Chianelli <chris.chianelli@gmail.com>
2020-03-11 13:47:33 -0400

    Add org.kie.kogito to scanBasePackages since Kogito handlers are added there.
    When I created an example using the kogito-spring-boot archetype, I noticed
    the handlers field of my BPMN process was null. After some investigating,
    I learned that scanBasePackages was only checking for my groupId (in
    fact, it added my groupId twice!). However, the kogito handlers
    are placed in org.kie.kogito.handlers, so it was not picking up the
    service handlers!
    
    This add org.kie.kogito.** to the maven archetype so it can detect
    the package.
    
    Alternatives:
    
    - Move Handlers to the package of the service they are a proxy of

--- archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/main/java/KogitoApplication.java
+++ archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/main/java/KogitoApplication.java
@@ -3,7 +3,7 @@ package ${package};
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"${package}.**", "${groupId}.**"})
+@SpringBootApplication(scanBasePackages = {"org.kie.kogito.**", "${package}.**", "${groupId}.**"})
 public class KogitoApplication {
 
     public static void main(String[] args) {
