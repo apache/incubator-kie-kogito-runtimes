1779c5327a6f1117f49f1933c071711981d173e4
jake <jacopo.r00ta@gmail.com>
2020-03-16 09:08:55 +0100

    restart jenkins

--- addons/monitoring-prometheus-addon/src/main/java/org/kie/addons/monitoring/system/metrics/dmnhandlers/DecisionConstants.java
+++ addons/monitoring-prometheus-addon/src/main/java/org/kie/addons/monitoring/system/metrics/dmnhandlers/DecisionConstants.java
@@ -19,9 +19,13 @@ public class DecisionConstants {
 
     public static final String DECISIONS_NAME_SUFFIX = "_dmn_result";
     public static final String DECISIONS_HELP = "Decision output.";
-    // Array of label names for a prometheus object that needs an handler and an identifier.
+    /**
+     * Array of label names for a prometheus object that needs an handler and an identifier.
+     */
     public static final String[] DECISION_ENDPOINT_IDENTIFIER_LABELS = new String[]{"decision", "endpoint", "identifier"};
-    // Array of label names for a prometheus object that needs only the handler.
+    /**
+     * Array of label names for a prometheus object that needs only the handler.
+     */
     public static final String[] DECISION_ENDPOINT_LABELS = new String[]{"decision", "endpoint"};
 
     private DecisionConstants() {
