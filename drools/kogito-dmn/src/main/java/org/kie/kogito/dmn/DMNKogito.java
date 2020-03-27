469fbb71a58ea9ff90a5de4962599944a546730e
tarilabs <matteo.mortari@gmail.com>
2020-03-27 15:49:13 +0100

    KOGITO-1575 DMN assembler-free ikb-free DMNRuntime

--- drools/kogito-dmn/src/main/java/org/kie/kogito/dmn/DMNKogito.java
+++ drools/kogito-dmn/src/main/java/org/kie/kogito/dmn/DMNKogito.java
@@ -16,7 +16,6 @@
 
 package org.kie.kogito.dmn;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -27,23 +26,23 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.drools.core.RuleBaseConfiguration;
-import org.drools.core.definitions.InternalKnowledgePackage;
-import org.drools.core.definitions.ResourceTypePackageRegistry;
-import org.drools.core.definitions.impl.KnowledgePackageImpl;
-import org.drools.core.impl.KnowledgeBaseImpl;
-import org.kie.api.io.ResourceType;
+import org.drools.core.io.impl.FileSystemResource;
+import org.drools.core.io.impl.ReaderResource;
+import org.kie.api.io.Resource;
 import org.kie.dmn.api.core.DMNModel;
 import org.kie.dmn.api.core.DMNRuntime;
-import org.kie.dmn.core.compiler.DMNCompilerImpl;
-import org.kie.dmn.core.impl.DMNPackageImpl;
-import org.kie.dmn.core.impl.DMNRuntimeImpl;
 import org.kie.dmn.core.internal.utils.DMNEvaluationUtils;
 import org.kie.dmn.core.internal.utils.DMNEvaluationUtils.DMNEvaluationResult;
+import org.kie.dmn.core.internal.utils.DMNRuntimeBuilder;
+import org.kie.kogito.Application;
 import org.kie.kogito.dmn.rest.DMNResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Internal Utility class.<br/>
+ * Use {@link Application#decisionModels()} of Kogito API to programmatically access DMN assets and evaluate DMN decisions.
+ */
 public class DMNKogito {
 
     private static final Logger LOG = LoggerFactory.getLogger(DMNKogito.class);
@@ -52,44 +51,41 @@ public class DMNKogito {
         // intentionally private.
     }
 
-    public static DMNRuntime createGenericDMNRuntime(Reader... resources) {
-        KnowledgeBaseImpl knowledgeBase = new KnowledgeBaseImpl("", new RuleBaseConfiguration());
-        Map<String, InternalKnowledgePackage> pkgs = knowledgeBase.getPackagesMap();
-        DMNCompilerImpl compilerImpl = new DMNCompilerImpl();
-        for (Reader dmnResource : resources) {
-            try {
-                DMNModel m = compilerImpl.compile(dmnResource);
-                InternalKnowledgePackage pkg = pkgs.computeIfAbsent(m.getNamespace(), KnowledgePackageImpl::new);
-                ResourceTypePackageRegistry rpkg = pkg.getResourceTypePackages();
-                DMNPackageImpl dmnpkg = rpkg.computeIfAbsent(ResourceType.DMN, rtp -> new DMNPackageImpl(m.getNamespace()));
-                dmnpkg.addModel(m.getName(), m);// TODO add profiles? and check dups over namespace/name
-            } catch (Exception e) {
-                LOG.error("Failed on DMN resource", e);
-            }
-        }
-        return new DMNRuntimeImpl(knowledgeBase);
+    /**
+     * Internal Utility class.<br/>
+     * Use {@link Application#decisionModels()} of Kogito API to programmatically access DMN assets and evaluate DMN decisions.
+     */
+    public static DMNRuntime createGenericDMNRuntime(Reader... readers) {
+        List<Resource> resources = Stream.of(readers).map(ReaderResource::new).collect(Collectors.toList());
+        DMNRuntime dmnRuntime = DMNRuntimeBuilder.fromDefaults()
+                                                 .setRootClassLoader(null)
+                                                 .buildConfiguration()
+                                                 .fromResources(resources)
+                                                 .getOrElseThrow(e -> new RuntimeException("Error initalizing DMNRuntime", e));
+        return dmnRuntime;
     }
 
+    /**
+     * Internal Utility class.<br/>
+     * Use {@link Application#decisionModels()} of Kogito API to programmatically access DMN assets and evaluate DMN decisions.
+     */
     public static DMNRuntime createGenericDMNRuntime() {
-        KnowledgeBaseImpl knowledgeBase = new KnowledgeBaseImpl("", new RuleBaseConfiguration());
-        Map<String, InternalKnowledgePackage> pkgs = knowledgeBase.getPackagesMap();
-        DMNCompilerImpl compilerImpl = new DMNCompilerImpl();
         try (Stream<Path> fileStream = Files.walk(Paths.get("."))) {
-            List<java.nio.file.Path> files = fileStream
-                                                       .filter(path -> Files.isRegularFile(path) && path.toString().endsWith(".dmn"))
-                                                       .peek(x -> LOG.debug("Adding DMN model {} to runtime", x))
-                                                       .collect(Collectors.toList());
-            for (java.nio.file.Path file : files) {
-                DMNModel m = compilerImpl.compile(new FileReader(file.toFile()));
-                InternalKnowledgePackage pkg = pkgs.computeIfAbsent(m.getNamespace(), KnowledgePackageImpl::new);
-                ResourceTypePackageRegistry rpkg = pkg.getResourceTypePackages();
-                DMNPackageImpl dmnpkg = rpkg.computeIfAbsent(ResourceType.DMN, rtp -> new DMNPackageImpl(m.getNamespace()));
-                dmnpkg.addModel(m.getName(), m);// TODO add profiles? and check dups over namespace/name
-            }
+            List<Resource> resources = fileStream.filter(path -> Files.isRegularFile(path) && path.toString().endsWith(".dmn"))
+                                                 .peek(x -> LOG.debug("Adding DMN model {} to runtime", x))
+                                                 .map(Path::toFile)
+                                                 .map(FileSystemResource::new)
+                                                 .collect(Collectors.toList());
+            DMNRuntime dmnRuntime = DMNRuntimeBuilder.fromDefaults()
+                                                     .setRootClassLoader(null)
+                                                     .buildConfiguration()
+                                                     .fromResources(resources)
+                                                     .getOrElseThrow(e -> new RuntimeException("Error initalizing DMNRuntime", e));
+            return dmnRuntime;
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException("Error initalizing DMNRuntime", e);
         }
-        return new DMNRuntimeImpl(knowledgeBase);
     }
 
     public static DMNModel modelByName(DMNRuntime dmnRuntime, String modelName) {
