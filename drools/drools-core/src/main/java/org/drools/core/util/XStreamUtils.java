0f3318de7bebdf3c18f3de58a464ce9b5fc731dc
tarilabs <matteo.mortari@gmail.com>
2020-04-27 20:32:45 +0200

    a test

--- drools/drools-core/src/main/java/org/drools/core/util/XStreamUtils.java
+++ drools/drools-core/src/main/java/org/drools/core/util/XStreamUtils.java
@@ -30,77 +30,222 @@ import com.thoughtworks.xstream.security.AnyTypePermission;
 import com.thoughtworks.xstream.security.TypePermission;
 import com.thoughtworks.xstream.security.WildcardTypePermission;
 
+import static com.thoughtworks.xstream.XStream.setupDefaultSecurity;
+
 public class XStreamUtils {
-    private static final String[] WHITELISTED_PACKAGES = new String[]{"org.drools.core.command.**", "org.drools.core.runtime.impl.ExecutionResultImpl", "org.drools.core.runtime.rule.impl.FlatQueryResults", "org.drools.core.common.DefaultFactHandle", "org.drools.core.common.EventFactHandle"};
 
-    private XStreamUtils() { }
+    private static final String[] WHITELISTED_PACKAGES = new String[] {
+        "org.drools.core.command.**",
+        "org.drools.core.runtime.impl.ExecutionResultImpl",
+        "org.drools.core.runtime.rule.impl.FlatQueryResults",
+        "org.drools.core.common.DefaultFactHandle",
+        "org.drools.core.common.EventFactHandle"
+    };
 
-    public static XStream createTrustingXStream() {
-        return internalCreateTrustingXStream(new XStream());
+    /**
+     * Vulnerable to CVE-210137285 variants. Do not use. Will be removed in the next few days!
+     * @deprecated in favor of {@link #createTrustingXStream()} and {@link #createNonTrustingXStream()}
+     */
+    @Deprecated
+    public static XStream createXStream() {
+        return internalCreateXStream( new XStream() );
     }
 
-    public static XStream createTrustingXStream(HierarchicalStreamDriver hierarchicalStreamDriver) {
-        return internalCreateTrustingXStream(new XStream(hierarchicalStreamDriver));
+    /**
+     * Vulnerable to CVE-210137285 variants. Do not use. Will be removed in the next few days!
+     * @deprecated in favor of {@link #createTrustingXStream()} and {@link #createNonTrustingXStream()}
+     */
+    @Deprecated
+    public static XStream createXStream(HierarchicalStreamDriver hierarchicalStreamDriver) {
+        return internalCreateXStream( new XStream(hierarchicalStreamDriver) );
     }
 
-    public static XStream createTrustingXStream(HierarchicalStreamDriver hierarchicalStreamDriver, ClassLoader classLoader) {
-        return internalCreateTrustingXStream(new XStream((ReflectionProvider)null, hierarchicalStreamDriver, new ClassLoaderReference(classLoader)));
+    /**
+     * Vulnerable to CVE-210137285 variants. Do not use. Will be removed in the next few days!
+     * @deprecated in favor of {@link #createTrustingXStream()} and {@link #createNonTrustingXStream()}
+     */
+    @Deprecated
+    public static XStream createXStream(HierarchicalStreamDriver hierarchicalStreamDriver, ClassLoader classLoader) {
+        return internalCreateXStream( new XStream(null, hierarchicalStreamDriver, new ClassLoaderReference( classLoader )) );
     }
 
-    public static XStream createTrustingXStream(ReflectionProvider reflectionProvider) {
-        return internalCreateTrustingXStream(new XStream(reflectionProvider));
+    /**
+     * Vulnerable to CVE-210137285 variants. Do not use. Will be removed in the next few days!
+     * @deprecated in favor of {@link #createTrustingXStream()} and {@link #createNonTrustingXStream()}
+     */
+    @Deprecated
+    public static XStream createXStream(ReflectionProvider reflectionProvider ) {
+        return internalCreateXStream( new XStream(reflectionProvider) );
     }
 
-    public static XStream createTrustingXStream(ReflectionProvider reflectionProvider, HierarchicalStreamDriver hierarchicalStreamDriver) {
-        return internalCreateTrustingXStream(new XStream(reflectionProvider, hierarchicalStreamDriver));
-    }
-
-    public static XStream createTrustingXStream(ReflectionProvider reflectionProvider, final Function<MapperWrapper, MapperWrapper> mapper) {
-        return internalCreateTrustingXStream(new XStream(reflectionProvider) {
+    /**
+     * Vulnerable to CVE-210137285 variants. Do not use. Will be removed in the next few days!
+     * @deprecated in favor of {@link #createTrustingXStream()} and {@link #createNonTrustingXStream()}
+     */
+    @Deprecated
+    public static XStream createXStream(ReflectionProvider reflectionProvider, Function<MapperWrapper, MapperWrapper> mapper ) {
+        return internalCreateXStream( new XStream(reflectionProvider) {
             protected MapperWrapper wrapMapper(MapperWrapper next) {
-                return (MapperWrapper)mapper.apply(next);
+                return mapper.apply( next );
             }
         });
     }
 
-    private static XStream internalCreateTrustingXStream(XStream xstream) {
-        XStream.setupDefaultSecurity(xstream);
+    /**
+     * Vulnerable to CVE-210137285 variants. Do not use. Will be removed in the next few days!
+     * @deprecated in favor of {@link #createTrustingXStream()} and {@link #createNonTrustingXStream()}
+     */
+    @Deprecated
+    private static XStream internalCreateXStream( XStream xstream ) {
+        setupDefaultSecurity(xstream);
+        xstream.addPermission( new WildcardTypePermission( new String[] {
+                "java.**", "javax.**", "org.kie.**", "org.drools.**", "org.jbpm.**", "org.optaplanner.**", "org.appformer.**"
+        } ) );
+        return xstream;
+    }
+
+    /**
+     * Only use for XML or JSON that comes from a 100% trusted source.
+     * The XML/JSON must be as safe as executable java code.
+     * Otherwise, you MUST use {@link #createNonTrustingXStream()}.
+     */
+    public static XStream createTrustingXStream() {
+        return internalCreateTrustingXStream( new XStream() );
+    }
+
+    /**
+     * Only use for XML or JSON that comes from a 100% trusted source.
+     * The XML/JSON must be as safe as executable java code.
+     * Otherwise, you MUST use {@link #createNonTrustingXStream()}.
+     */
+    public static XStream createTrustingXStream(HierarchicalStreamDriver hierarchicalStreamDriver) {
+        return internalCreateTrustingXStream( new XStream(hierarchicalStreamDriver) );
+    }
+
+    /**
+     * Only use for XML or JSON that comes from a 100% trusted source.
+     * The XML/JSON must be as safe as executable java code.
+     * Otherwise, you MUST use {@link #createNonTrustingXStream()}.
+     */
+    public static XStream createTrustingXStream(HierarchicalStreamDriver hierarchicalStreamDriver, ClassLoader classLoader) {
+        return internalCreateTrustingXStream( new XStream(null, hierarchicalStreamDriver, new ClassLoaderReference( classLoader )) );
+    }
+
+    /**
+     * Only use for XML or JSON that comes from a 100% trusted source.
+     * The XML/JSON must be as safe as executable java code.
+     * Otherwise, you MUST use {@link #createNonTrustingXStream()}.
+     */
+    public static XStream createTrustingXStream(ReflectionProvider reflectionProvider ) {
+        return internalCreateTrustingXStream( new XStream(reflectionProvider) );
+    }
+    
+    /**
+     * Only use for XML or JSON that comes from a 100% trusted source.
+     * The XML/JSON must be as safe as executable java code.
+     * Otherwise, you MUST use {@link #createNonTrustingXStream()}.
+     */
+    public static XStream createTrustingXStream(ReflectionProvider reflectionProvider, HierarchicalStreamDriver hierarchicalStreamDriver ) {
+        return internalCreateTrustingXStream( new XStream(reflectionProvider, hierarchicalStreamDriver) );
+    }
+
+    /**
+     * Only use for XML or JSON that comes from a 100% trusted source.
+     * The XML/JSON must be as safe as executable java code.
+     * Otherwise, you MUST use {@link #createNonTrustingXStream()}.
+     */
+    public static XStream createTrustingXStream(ReflectionProvider reflectionProvider, Function<MapperWrapper, MapperWrapper> mapper ) {
+        return internalCreateTrustingXStream( new XStream(reflectionProvider) {
+            protected MapperWrapper wrapMapper(MapperWrapper next) {
+                return mapper.apply( next );
+            }
+        });
+    }
+
+    /**
+     * Only use for XML or JSON that comes from a 100% trusted source.
+     * The XML/JSON must be as safe as executable java code.
+     * Otherwise, you MUST use {@link #createNonTrustingXStream()}.
+     */
+    private static XStream internalCreateTrustingXStream( XStream xstream ) {
+        setupDefaultSecurity(xstream);
+        // Presumes the XML content comes from a trusted source!
         xstream.addPermission(new AnyTypePermission());
         return xstream;
     }
 
+    /**
+     * Use for XML or JSON that might not come from a trusted source (such as REST services payloads, ...).
+     * Automatically whitelists all classes with an {@link XStreamAlias} annotation.
+     * Often requires whitelisting additional domain specific classes, which you'll need to expose in your API's.
+     */
     public static XStream createNonTrustingXStream() {
-        return internalCreateNonTrustingXStream(new XStream());
+        return internalCreateNonTrustingXStream( new XStream() );
     }
 
+    /**
+     * Use for XML or JSON that might not come from a trusted source (such as REST services payloads, ...).
+     * Automatically whitelists all classes with an {@link XStreamAlias} annotation.
+     * Often requires whitelisting additional domain specific classes, which you'll need to expose in your API's.
+     */
     public static XStream createNonTrustingXStream(HierarchicalStreamDriver hierarchicalStreamDriver) {
-        return internalCreateNonTrustingXStream(new XStream(hierarchicalStreamDriver));
+        return internalCreateNonTrustingXStream( new XStream(hierarchicalStreamDriver) );
     }
 
+    /**
+     * Use for XML or JSON that might not come from a trusted source (such as REST services payloads, ...).
+     * Automatically whitelists all classes with an {@link XStreamAlias} annotation.
+     * Often requires whitelisting additional domain specific classes, which you'll need to expose in your API's.
+     */
     public static XStream createNonTrustingXStream(HierarchicalStreamDriver hierarchicalStreamDriver, ClassLoader classLoader) {
-        return internalCreateNonTrustingXStream(new XStream((ReflectionProvider)null, hierarchicalStreamDriver, new ClassLoaderReference(classLoader)));
+        return internalCreateNonTrustingXStream( new XStream(null, hierarchicalStreamDriver, new ClassLoaderReference( classLoader )) );
     }
 
-    public static XStream createNonTrustingXStream(ReflectionProvider reflectionProvider) {
-        return internalCreateNonTrustingXStream(new XStream(reflectionProvider));
+    /**
+     * Use for XML or JSON that might not come from a trusted source (such as REST services payloads, ...).
+     * Automatically whitelists all classes with an {@link XStreamAlias} annotation.
+     * Often requires whitelisting additional domain specific classes, which you'll need to expose in your API's.
+     */
+    public static XStream createNonTrustingXStream(ReflectionProvider reflectionProvider ) {
+        return internalCreateNonTrustingXStream( new XStream(reflectionProvider) );
+    }
+    
+    /**
+     * Use for XML or JSON that might not come from a trusted source (such as REST services payloads, ...).
+     * Automatically whitelists all classes with an {@link XStreamAlias} annotation.
+     * Often requires whitelisting additional domain specific classes, which you'll need to expose in your API's.
+     */
+    public static XStream createNonTrustingXStream(ReflectionProvider reflectionProvider, HierarchicalStreamDriver hierarchicalStreamDriver ) {
+        return internalCreateNonTrustingXStream( new XStream(reflectionProvider, hierarchicalStreamDriver) );
     }
 
-    public static XStream createNonTrustingXStream(ReflectionProvider reflectionProvider, HierarchicalStreamDriver hierarchicalStreamDriver) {
-        return internalCreateNonTrustingXStream(new XStream(reflectionProvider, hierarchicalStreamDriver));
-    }
-
-    public static XStream createNonTrustingXStream(ReflectionProvider reflectionProvider, final Function<MapperWrapper, MapperWrapper> mapper) {
-        return internalCreateNonTrustingXStream(new XStream(reflectionProvider) {
+    /**
+     * Use for XML or JSON that might not come from a trusted source (such as REST services payloads, ...).
+     * Automatically whitelists all classes with an {@link XStreamAlias} annotation.
+     * Often requires whitelisting additional domain specific classes, which you'll need to expose in your API's.
+     */
+    public static XStream createNonTrustingXStream(ReflectionProvider reflectionProvider, Function<MapperWrapper, MapperWrapper> mapper ) {
+        return internalCreateNonTrustingXStream( new XStream(reflectionProvider) {
             protected MapperWrapper wrapMapper(MapperWrapper next) {
-                return (MapperWrapper)mapper.apply(next);
+                return mapper.apply( next );
             }
         });
     }
 
-    private static XStream internalCreateNonTrustingXStream(XStream xstream) {
-        XStream.setupDefaultSecurity(xstream);
-        xstream.addPermission(new AnyAnnotationTypePermission());
-        xstream.addPermission(new WildcardTypePermission(WHITELISTED_PACKAGES));
+    /**
+     * Use for XML or JSON that might not come from a trusted source (such as REST services payloads, ...).
+     * Automatically whitelists all classes with an {@link XStreamAlias} annotation.
+     * Often requires whitelisting additional domain specific classes, which you'll need to expose in your API's.
+     */
+    private static XStream internalCreateNonTrustingXStream( XStream xstream ) {
+        setupDefaultSecurity(xstream);
+        // TODO remove if setupDefaultSecurity already does this.
+        // See comment in https://github.com/x-stream/xstream/pull/99
+        xstream.addPermission( new AnyAnnotationTypePermission());
+        xstream.addPermission( new WildcardTypePermission( WHITELISTED_PACKAGES ) );
+        // Do not add root permissions for "java", "org.kie" or the like here because that creates a security problem.
+        // For more information, see http://x-stream.github.io/security.html and various xstream dev list conversations.
+        // Instead, embrace a whitelist approach and expose that in your API's.
         return xstream;
     }
 
