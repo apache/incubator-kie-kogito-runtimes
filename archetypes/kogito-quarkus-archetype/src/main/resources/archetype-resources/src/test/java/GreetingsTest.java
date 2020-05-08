3a36a485cb9d0ec4c478d1b8121f739da20f2adf
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-05-08 15:14:51 +0200

    add tests

--- archetypes/kogito-quarkus-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
+++ archetypes/kogito-quarkus-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
@@ -1,12 +1,13 @@
 package ${package};
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 
 @QuarkusTest
 public class GreetingsTest {
@@ -14,10 +15,13 @@ public class GreetingsTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/greetings")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body("{}")
+        .when()
+            .post("/greetings")
+        .then()
+            .body("id", notNullValue());    
     }
 
 }
