75afd18633689b4d55da5c19d07d49525c769e4c
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-05-11 15:12:07 +0200

    add rest assured for spring

--- archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
+++ archetypes/kogito-springboot-archetype/src/main/resources/archetype-resources/src/test/java/GreetingsTest.java
@@ -1,47 +1,38 @@
 package ${package};
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoApplication.class)
-@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // reset spring context after each test method
 public class GreetingsTest {
 
-    @Autowired
-    private WebApplicationContext wac;
-
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-    }
-
+    @LocalServerPort
+    int randomServerPort;
 
     @Test
     public void testOrderProcess() throws Exception {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/greetings")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .content("{}"))
-            .andExpect(status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.id").isNotEmpty());
-
+        RestAssured.port = randomServerPort;
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("{}")
+                .when()
+                .post("/greetings")
+                .then()
+                .statusCode(200)
+                .body("id", notNullValue());
     }
 
 }
