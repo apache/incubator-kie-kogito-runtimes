8a324f3afb4d964d6221b6ad0810b4b3fd030fd4
Luca Molteni <volothamp@gmail.com>
2020-05-12 14:19:18 +0200

    Fix compilation

--- kogito-codegen/src/main/java/org/kie/kogito/codegen/decision/DecisionCodegen.java
+++ kogito-codegen/src/main/java/org/kie/kogito/codegen/decision/DecisionCodegen.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
