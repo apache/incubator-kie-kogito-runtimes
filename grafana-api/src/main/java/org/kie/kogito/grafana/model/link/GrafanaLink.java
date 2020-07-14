773c2b86d8db7b1ba81ea1ab561cd932eeb73945
jake <jacopo.r00ta@gmail.com>
2020-07-14 09:47:47 +0200

    add licence header, add tests

--- grafana-api/src/main/java/org/kie/kogito/grafana/model/link/GrafanaLink.java
+++ grafana-api/src/main/java/org/kie/kogito/grafana/model/link/GrafanaLink.java
@@ -1,11 +1,26 @@
+/*
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.kie.kogito.grafana.model.link;
 
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 public class GrafanaLink {
+
     @JsonProperty("icon")
     public String icon = "external link";
 
@@ -27,7 +42,10 @@ public class GrafanaLink {
     @JsonProperty("url")
     public String url;
 
-    public GrafanaLink(String title, String url){
+    public GrafanaLink() {
+    }
+
+    public GrafanaLink(String title, String url) {
         this.title = title;
         this.url = url;
     }
