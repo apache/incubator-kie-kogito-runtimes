da00b5bc1374349d54e94b64fc81c6dafafc8bb7
mariofusco <mario.fusco@gmail.com>
2020-07-15 13:51:54 +0200

    compiler unfork

--- drools/drools-compiler/src/main/java/org/drools/compiler/builder/impl/KnowledgeBuilderFactoryServiceImpl.java
+++ drools/drools-compiler/src/main/java/org/drools/compiler/builder/impl/KnowledgeBuilderFactoryServiceImpl.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2005 JBoss Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ * You may obtain a copy of the License at
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -11,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package org.drools.compiler.builder.impl;
 
@@ -28,15 +29,15 @@ import org.kie.internal.builder.KnowledgeBuilderFactoryService;
 import org.kie.internal.builder.ScoreCardConfiguration;
 
 public class KnowledgeBuilderFactoryServiceImpl implements KnowledgeBuilderFactoryService {
-    
+
     public KnowledgeBuilderConfiguration newKnowledgeBuilderConfiguration() {
         return new KnowledgeBuilderConfigurationImpl();
     }
-    
-    public KnowledgeBuilderConfiguration newKnowledgeBuilderConfiguration(Properties properties, ClassLoader... classLoaders) {
-        return new KnowledgeBuilderConfigurationImpl(properties, classLoaders);
+
+    public KnowledgeBuilderConfiguration newKnowledgeBuilderConfiguration( Properties properties, ClassLoader... classLoaders ) {
+        return new KnowledgeBuilderConfigurationImpl( properties, classLoaders );
     }
-    
+
     public DecisionTableConfiguration newDecisionTableConfiguration() {
         return new DecisionTableConfigurationImpl();
     }
@@ -46,28 +47,27 @@ public class KnowledgeBuilderFactoryServiceImpl implements KnowledgeBuilderFacto
     }
 
     public KnowledgeBuilder newKnowledgeBuilder() {
-        return new KnowledgeBuilderImpl( );
+        return new KnowledgeBuilderImpl();
     }
 
-    public KnowledgeBuilder newKnowledgeBuilder(KnowledgeBuilderConfiguration conf) {
-        return new KnowledgeBuilderImpl( (KnowledgeBuilderConfigurationImpl) conf );
+    public KnowledgeBuilder newKnowledgeBuilder( KnowledgeBuilderConfiguration conf ) {
+        return new KnowledgeBuilderImpl( ( KnowledgeBuilderConfigurationImpl ) conf );
     }
 
-    public KnowledgeBuilder newKnowledgeBuilder(KieBase kbase) {
+    public KnowledgeBuilder newKnowledgeBuilder( KieBase kbase ) {
         if ( kbase != null ) {
-            return new KnowledgeBuilderImpl( (InternalKnowledgeBase)kbase );
+            return new KnowledgeBuilderImpl( ( InternalKnowledgeBase ) kbase );
         } else {
             return new KnowledgeBuilderImpl();
         }
     }
 
-    public KnowledgeBuilder newKnowledgeBuilder(KieBase kbase,
-                                                KnowledgeBuilderConfiguration conf) {
+    public KnowledgeBuilder newKnowledgeBuilder( KieBase kbase,
+                                                 KnowledgeBuilderConfiguration conf ) {
         if ( kbase != null ) {
-            return new KnowledgeBuilderImpl( (InternalKnowledgeBase)kbase, (KnowledgeBuilderConfigurationImpl) conf );
+            return new KnowledgeBuilderImpl( ( InternalKnowledgeBase ) kbase, ( KnowledgeBuilderConfigurationImpl ) conf );
         } else {
-            return new KnowledgeBuilderImpl((KnowledgeBuilderConfigurationImpl) conf );
-        }        
+            return new KnowledgeBuilderImpl( ( KnowledgeBuilderConfigurationImpl ) conf );
+        }
     }
-
 }
--- drools/drools-compiler/src/test/java/org/drools/compiler/integrationtests/SerializationHelper.java
+++ drools/drools-compiler/src/test/java/org/drools/compiler/integrationtests/SerializationHelper.java
@@ -43,36 +43,36 @@ public class SerializationHelper {
     public static <T> T serializeObject(final T obj,
                                         final ClassLoader classLoader) throws IOException,
             ClassNotFoundException {
-        return (T) DroolsStreamUtils.streamIn(DroolsStreamUtils.streamOut(obj), classLoader);
+        return (T) DroolsStreamUtils.streamIn( DroolsStreamUtils.streamOut(obj), classLoader);
     }
 
-    public static StatefulKnowledgeSession getSerialisedStatefulKnowledgeSession(final KieSession ksession,
-                                                                                 final boolean dispose) throws Exception {
+    public static StatefulKnowledgeSession getSerialisedStatefulKnowledgeSession( final KieSession ksession,
+                                                                                  final boolean dispose) throws Exception {
         return getSerialisedStatefulKnowledgeSession(ksession,
                                                      dispose,
                                                      true);
     }
 
-    public static StatefulKnowledgeSession getSerialisedStatefulKnowledgeSession(final KieSession ksession,
-                                                                                 final boolean dispose,
-                                                                                 final boolean testRoundTrip) throws Exception {
+    public static StatefulKnowledgeSession getSerialisedStatefulKnowledgeSession( final KieSession ksession,
+                                                                                  final boolean dispose,
+                                                                                  final boolean testRoundTrip) throws Exception {
         return getSerialisedStatefulKnowledgeSession(ksession, ksession.getKieBase(), dispose);
     }
 
-    public static StatefulKnowledgeSession getSerialisedStatefulKnowledgeSession(final KieSession ksession,
-                                                                                 final KieBase kbase,
-                                                                                 final boolean dispose) throws Exception {
+    public static StatefulKnowledgeSession getSerialisedStatefulKnowledgeSession( final KieSession ksession,
+                                                                                  final KieBase kbase,
+                                                                                  final boolean dispose) throws Exception {
         return getSerialisedStatefulKnowledgeSessionWithMessage(ksession, kbase, dispose).getSession();
     }
 
-    public static ReadSessionResult getSerialisedStatefulKnowledgeSessionWithMessage(final KieSession ksession,
-                                                                                 final KieBase kbase,
-                                                                                 final boolean dispose) throws Exception {
-        final ProtobufMarshaller marshaller = (ProtobufMarshaller) MarshallerFactory.newMarshaller(kbase,
-                                                                                                   (ObjectMarshallingStrategy[]) ksession.getEnvironment().get(EnvironmentName.OBJECT_MARSHALLING_STRATEGIES));
+    public static ReadSessionResult getSerialisedStatefulKnowledgeSessionWithMessage( final KieSession ksession,
+                                                                                      final KieBase kbase,
+                                                                                      final boolean dispose) throws Exception {
+        final ProtobufMarshaller marshaller = ( ProtobufMarshaller ) MarshallerFactory.newMarshaller(kbase,
+                                                                                                   ( ObjectMarshallingStrategy[]) ksession.getEnvironment().get( EnvironmentName.OBJECT_MARSHALLING_STRATEGIES));
         final long time = ksession.getSessionClock().getCurrentTime();
         // make sure globas are in the environment of the session
-        ksession.getEnvironment().set(EnvironmentName.GLOBALS, ksession.getGlobals());
+        ksession.getEnvironment().set( EnvironmentName.GLOBALS, ksession.getGlobals());
 
         // Serialize object
         final byte[] serializedObject;
