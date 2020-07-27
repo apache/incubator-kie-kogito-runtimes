245c35876b6a6d588274d12e474f05e7118758dd
mariofusco <mario.fusco@gmail.com>
2020-07-27 11:51:34 +0200

    wip

--- drools/drools-core/src/main/java/org/drools/core/common/KogitoPhreakWorkingMemoryFactory.java
+++ drools/drools-core/src/main/java/org/drools/core/common/KogitoPhreakWorkingMemoryFactory.java
@@ -1,9 +1,9 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2020 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 
 package org.drools.core.common;
 
