gustavonalle/kogito-runtimes

KOGITO-2911 Refactor Codegen for "RuleUnits" class in Application.java

kogito-codegen/src/main/resources/class-templates/rules/RuleUnitTemplate.java
@@ -1,13 +1,19 @@
 package $Package$;
 
+import org.drools.core.ClockType;
+import org.drools.core.RuleBaseConfiguration;
+import org.drools.core.SessionConfigurationImpl;
+import org.drools.core.impl.EnvironmentImpl;
+import org.kie.api.conf.EventProcessingOption;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.KieSessionsPool;
 import org.kie.kogito.rules.RuleEventListenerConfig;
 import org.kie.kogito.rules.units.impl.AbstractRuleUnit;
 
 public class $Name$ extends AbstractRuleUnit<$ModelName$> {
 
     public $Name$(org.kie.kogito.Application app) {
-        super(app);
+        super($ModelName$.class.getCanonicalName(), app);
     }
 
     public $InstanceName$ internalCreateInstance($ModelName$ value) {
@@ -15,10 +21,23 @@ public class $Name$ extends AbstractRuleUnit<$ModelName$> {
     }
 
     private KieSession createLegacySession() {
-        KieSession ks = app.ruleUnits().ruleRuntimeBuilder().newKieSession( $ModelClass$ );
+        RuleBaseConfiguration ruleBaseConfig = new RuleBaseConfiguration();
+        ruleBaseConfig.setEventProcessingMode($EventProcessingMode$);
+        ruleBaseConfig.setSessionPoolSize($SessionPoolSize$);
+        org.drools.core.impl.InternalKnowledgeBase kb =
+                org.drools.modelcompiler.builder.KieBaseBuilder.createKieBaseFromModel(
+                        new $RuleModelName$(), ruleBaseConfig);
+
+        SessionConfigurationImpl sessionConfig = new SessionConfigurationImpl();
+        sessionConfig.setClockType($ClockType$);
+
+        KieSession ks = kb.newKieSession(sessionConfig, new EnvironmentImpl());
+        ((org.drools.core.impl.StatefulKnowledgeSessionImpl)ks).setStateless( /*$IsStateful$*/ true );
         ((org.drools.core.impl.StatefulKnowledgeSessionImpl)ks).setApplication( app );
-        if (app.config() != null && app.config().rule() != null) {
-            RuleEventListenerConfig ruleEventListenerConfig = app.config().rule().ruleEventListeners();
+
+        org.kie.kogito.Config cfg = app.config();
+        if (cfg != null) {
+            RuleEventListenerConfig ruleEventListenerConfig = cfg.rule().ruleEventListeners();
             ruleEventListenerConfig.agendaListeners().forEach(ks::addEventListener);
             ruleEventListenerConfig.ruleRuntimeListeners().forEach(ks::addEventListener);
         }

apache/incubator-kie-kogito-runtimes

kogito-codegen/src/main/resources/class-templates/rules/RuleUnitTemplate.java
@@ -1,13 +1,19 @@
 package $Package$;
 
+import org.drools.core.ClockType;
+import org.drools.core.RuleBaseConfiguration;
+import org.drools.core.SessionConfigurationImpl;
+import org.drools.core.impl.EnvironmentImpl;
+import org.kie.api.conf.EventProcessingOption;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.KieSessionsPool;
 import org.kie.kogito.rules.RuleEventListenerConfig;
 import org.kie.kogito.rules.units.impl.AbstractRuleUnit;
 
 public class $Name$ extends AbstractRuleUnit<$ModelName$> {
 
     public $Name$(org.kie.kogito.Application app) {
-        super(app);
+        super($ModelName$.class.getCanonicalName(), app);
     }
 
     public $InstanceName$ internalCreateInstance($ModelName$ value) {
@@ -15,10 +21,23 @@ public class $Name$ extends AbstractRuleUnit<$ModelName$> {
     }
 
     private KieSession createLegacySession() {
-        KieSession ks = app.ruleUnits().ruleRuntimeBuilder().newKieSession( $ModelClass$ );
+        RuleBaseConfiguration ruleBaseConfig = new RuleBaseConfiguration();
+        ruleBaseConfig.setEventProcessingMode($EventProcessingMode$);
+        ruleBaseConfig.setSessionPoolSize($SessionPoolSize$);
+        org.drools.core.impl.InternalKnowledgeBase kb =
+                org.drools.modelcompiler.builder.KieBaseBuilder.createKieBaseFromModel(
+                        new $RuleModelName$(), ruleBaseConfig);
+
+        SessionConfigurationImpl sessionConfig = new SessionConfigurationImpl();
+        sessionConfig.setClockType($ClockType$);
+
+        KieSession ks = kb.newKieSession(sessionConfig, new EnvironmentImpl());
+        ((org.drools.core.impl.KogitoStatefulKnowledgeSessionImpl)ks).setStateless( /*$IsStateful$*/ true );
         ((org.drools.core.impl.KogitoStatefulKnowledgeSessionImpl)ks).setApplication( app );
-        if (app.config() != null && app.config().rule() != null) {
-            RuleEventListenerConfig ruleEventListenerConfig = app.config().rule().ruleEventListeners();
+
+        org.kie.kogito.Config cfg = app.config();
+        if (cfg != null) {
+            RuleEventListenerConfig ruleEventListenerConfig = cfg.rule().ruleEventListeners();
             ruleEventListenerConfig.agendaListeners().forEach(ks::addEventListener);
             ruleEventListenerConfig.ruleRuntimeListeners().forEach(ks::addEventListener);
         }