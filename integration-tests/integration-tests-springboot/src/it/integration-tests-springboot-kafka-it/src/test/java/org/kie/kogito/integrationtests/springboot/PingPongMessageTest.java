7d39846d71f6c6c2ae7295380a369975df2d24ef
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-10-30 12:34:54 +0100

    readd validation

--- integration-tests/integration-tests-springboot/src/it/integration-tests-springboot-kafka-it/src/test/java/org/kie/kogito/integrationtests/springboot/PingPongMessageTest.java
+++ integration-tests/integration-tests-springboot/src/it/integration-tests-springboot-kafka-it/src/test/java/org/kie/kogito/integrationtests/springboot/PingPongMessageTest.java
@@ -74,6 +74,8 @@ public class PingPongMessageTest extends BaseRestTest {
                 .statusCode(201)
                 .extract().body().path("id");
 
+        validateSubProcess();
+
         await().atMost(Duration.ofSeconds(5))
                 .untilAsserted(() -> given()
                         .contentType(ContentType.JSON)
@@ -99,5 +101,40 @@ public class PingPongMessageTest extends BaseRestTest {
 
         latch.await(5, TimeUnit.SECONDS);
 
+    }
+
+
+    private void validateSubProcess(){
+        await().atMost(Duration.ofSeconds(5))
+                .untilAsserted(() -> given()
+                        .contentType(ContentType.JSON)
+                        .when()
+                        .get("/pong_message/")
+                        .then()
+                        .statusCode(200)
+                        .body("$.size", equalTo(1)));
+
+        String pId = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/pong_message/")
+                .then()
+                .statusCode(200)
+                .body("$.size", equalTo(1))
+                .extract().body().path("[0].id");
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/pong_message/{pId}/end", pId)
+                .then()
+                .statusCode(200);
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/pong_message/{pId}", pId)
+                .then()
+                .statusCode(404);
     }
 }
