ae99a3cbaf89f273165df0270bd7e3a14f2471c8
Jacopo Rota <jacopo.r00ta@gmail.com>
2020-11-02 14:08:56 +0100

    Update QuarkusStartupModelPublisher.java

--- addons/tracing/tracing-decision-quarkus-addon/src/main/java/org/kie/kogito/tracing/decision/QuarkusStartupModelPublisher.java
+++ addons/tracing/tracing-decision-quarkus-addon/src/main/java/org/kie/kogito/tracing/decision/QuarkusStartupModelPublisher.java
@@ -34,6 +34,7 @@ public class QuarkusStartupModelPublisher {
     @Inject
     QuarkusModelEventEmitter modelEventEmitter;
 
+    // The bean that fires the event at startup must be separated from the kafka connector due to https://github.com/quarkusio/quarkus/issues/12820
     public void publish(@Observes StartupEvent event) {
         LOGGER.debug("Publishing decision models to the kafka topic");
         modelEventEmitter.publishDecisionModels();
