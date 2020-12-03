76fb931ccc33e75f525cac7cc501a5763546cb18
jake <jacopo.r00ta@gmail.com>
2020-12-03 10:15:19 +0100

    remove unused dependencies, refactoring

--- addons/monitoring-addon/monitoring-core/monitoring-core-springboot/src/main/java/org/kie/kogito/monitoring/core/springboot/SpringbootEventListenerFactory.java
+++ addons/monitoring-addon/monitoring-core/monitoring-core-springboot/src/main/java/org/kie/kogito/monitoring/core/springboot/SpringbootEventListenerFactory.java
@@ -17,7 +17,9 @@ package org.kie.kogito.monitoring.core.springboot;
 
 import org.drools.core.config.DefaultRuleEventListenerConfig;
 import org.kie.kogito.monitoring.core.common.Constants;
+import org.kie.kogito.monitoring.core.common.process.MonitoringProcessEventListenerConfig;
 import org.kie.kogito.monitoring.core.common.rule.RuleMetricsListenerConfig;
+import org.kie.kogito.process.impl.DefaultProcessEventListenerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -25,14 +27,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnProperty(
-        value = Constants.MONITORING_RULE_USE_DEFAULT,
-        havingValue = "true",
-        matchIfMissing = true)
-public class SpringbootRuleEventListenerFactory {
+public class SpringbootEventListenerFactory {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootRuleEventListenerFactory.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootEventListenerFactory.class);
 
+    @Bean
+    @ConditionalOnProperty(
+            value = Constants.MONITORING_PROCESS_USE_DEFAULT,
+            havingValue = "true",
+            matchIfMissing = true)
+    public DefaultProcessEventListenerConfig produceProcessListener() {
+        LOGGER.debug("Producing default listener for process monitoring.");
+        return new MonitoringProcessEventListenerConfig();
+    }
+
+    @ConditionalOnProperty(
+            value = Constants.MONITORING_RULE_USE_DEFAULT,
+            havingValue = "true",
+            matchIfMissing = true)
     @Bean
     public DefaultRuleEventListenerConfig produceRuleListener() {
         LOGGER.debug("Producing default listener for rule monitoring.");
