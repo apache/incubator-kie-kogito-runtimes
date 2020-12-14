ce55a30505a065f433aa913d8748bb057a9bc56f
danielezonca <dzonca@redhat.com>
2020-12-14 16:09:48 +0100

    After merge fix

--- api/kogito-api/src/main/java/org/kie/kogito/StaticApplication.java
+++ api/kogito-api/src/main/java/org/kie/kogito/StaticApplication.java
@@ -36,10 +36,6 @@ public class StaticApplication implements Application {
             KogitoEngine ... engines) {
         this.config = config;
         loadEngines(engines);
-
-        if (config() != null && config().process() != null) {
-            unitOfWorkManager().eventManager().setAddons(config().addons());
-        }
     }
 
     public Config config() {
