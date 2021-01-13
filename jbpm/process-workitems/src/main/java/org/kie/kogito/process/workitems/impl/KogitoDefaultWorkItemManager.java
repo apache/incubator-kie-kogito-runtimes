8dfba7440a9ca431e6f245185abc7e11da414b96
Edoardo Vacchi <uncommonnonsense@gmail.com>
2021-01-13 13:54:59 +0100

    cleanup

--- jbpm/process-workitems/src/main/java/org/kie/kogito/process/workitems/impl/KogitoDefaultWorkItemManager.java
+++ jbpm/process-workitems/src/main/java/org/kie/kogito/process/workitems/impl/KogitoDefaultWorkItemManager.java
@@ -15,9 +15,7 @@
 
 package org.kie.kogito.process.workitems.impl;
 
-import java.io.Externalizable;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.HashMap;
 import java.util.HashSet;
