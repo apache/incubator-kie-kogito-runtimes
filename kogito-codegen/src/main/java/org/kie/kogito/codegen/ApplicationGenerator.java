bc84959e06df1d40dd8e48d4ca5739b159af05f7
danielezonca <dzonca@redhat.com>
2021-01-15 12:23:56 +0100

    Minor

--- kogito-codegen/src/main/java/org/kie/kogito/codegen/ApplicationGenerator.java
+++ kogito-codegen/src/main/java/org/kie/kogito/codegen/ApplicationGenerator.java
@@ -24,7 +24,6 @@ import java.util.Collection;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Optional;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.drools.core.util.StringUtils;
