48fbca76aeada9552315c83c23bd192d3d179592
Alessandro Costa <alecosta.me@gmail.com>
2021-02-23 11:55:03 +0100

    KOGITO-4358: fix SpringBootTopicsInformationResourceTest

--- addons/cloudevents/cloudevents-spring-boot-addon-it/src/test/java/org/kie/kogito/integrationtests/SpringBootTopicsInformationResourceTest.java
+++ addons/cloudevents/cloudevents-spring-boot-addon-it/src/test/java/org/kie/kogito/integrationtests/SpringBootTopicsInformationResourceTest.java
@@ -18,23 +18,27 @@ package org.kie.kogito.integrationtests;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Disabled;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.kie.kogito.app.KogitoSpringbootApplication;
 import org.kie.kogito.event.ChannelType;
 import org.kie.kogito.event.Topic;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = KogitoSpringbootApplication.class)
 public class SpringBootTopicsInformationResourceTest {
 
+    @LocalServerPort
+    int randomServerPort;
+
     @Test
     public void test() {
+        RestAssured.port = randomServerPort;
         List<Topic> topicList = given()
                 .when()
                 .get("/messaging/topics")
