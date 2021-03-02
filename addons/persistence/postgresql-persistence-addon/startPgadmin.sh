8638a2e335eac02fda8f2aef2f7f2cda757b4760
Tiago Dolphine <tiagodolphine@gmail.com>
2021-03-02 14:41:14 -0300

    Adjusting vertx version in parent-pom and removing unused scripts

--- addons/persistence/postgresql-persistence-addon/startPgadmin.sh
+++
@@ -1,1 +0,0 @@
-docker run -p 8055:80 -e 'PGADMIN_DEFAULT_EMAIL=user@user.com' -e 'PGADMIN_DEFAULT_PASSWORD=pass' --name dev-pgadmin -d dpage/pgadmin4
--- addons/persistence/postgresql-persistence-addon/startPostgresql.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --name dev-postgres -e POSTGRES_PASSWORD=Pass2020! -v ${HOME}/dev/postgres-data/:/var/lib/postgresql/data -p 5432:5432 postgres
