swati-kale/kogito-runtimes

[KOGITO-4438] PostgreSQL research about doing optmistic locking
Signed-off-by: Swati Kale <swkale@redhat.com>

jbpm/jbpm-flow/src/main/java/org/jbpm/process/instance/ProcessInstanceManager.java
@@ -39,4 +39,6 @@ public interface ProcessInstanceManager {
 
     void clearProcessInstancesState();
 
+    void lock(boolean lock);
+
 }

apache/incubator-kie-kogito-runtimes

jbpm/jbpm-flow/src/main/java/org/jbpm/process/instance/ProcessInstanceManager.java
@@ -39,4 +39,6 @@ public interface ProcessInstanceManager {
 
     void clearProcessInstancesState();
 
+    void setLock(boolean lock);
+
 }