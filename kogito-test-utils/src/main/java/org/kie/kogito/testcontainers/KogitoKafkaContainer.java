283a1054144aa64b9f0e5611d85aab879ffaa182
Cristiano Nicolai <570894+cristianonicolai@users.noreply.github.com>
2021-08-25 17:42:37 +1000

    KOGITO-5664 - Disable Kafka metrics

--- kogito-test-utils/src/main/java/org/kie/kogito/testcontainers/KogitoKafkaContainer.java
+++ kogito-test-utils/src/main/java/org/kie/kogito/testcontainers/KogitoKafkaContainer.java
@@ -45,6 +45,8 @@ public class KogitoKafkaContainer extends KafkaContainer implements TestResource
         withEnv("KAFKA_OFFSETS_RETENTION_MINUTES", "1");
         withEnv("KAFKA_AUTO_LEADER_REBALANCE_ENABLE", "false");
         withEnv("KAFKA_CONFLUENT_SUPPORT_METRICS_ENABLE", "false");
+        withEnv("KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR", "1");
+        withEnv("KAFKA_AUTO_CREATE_TOPICS_ENABLED", "true");
     }
 
     @Override
