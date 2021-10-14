ad4ccea0fa53e2eb774b928925f9ef1ac630a9e3
Daniele Zonca <dzonca@redhat.com>
2021-10-14 14:38:06 +0200

    Update Person.java

--- integration-tests/integration-tests-quarkus-processes-persistence/integration-tests-processes-persistence-common/src/main/java/org/kie/kogito/Person.java
+++ integration-tests/integration-tests-quarkus-processes-persistence/integration-tests-processes-persistence-common/src/main/java/org/kie/kogito/Person.java
@@ -20,9 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection
 public class Person implements Serializable {
 
     private String id;
