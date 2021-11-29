e29baae5268f23c57b523ddd67ea29d28787d11e
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-11-29 10:02:24 +0100

    [KOGITO-5944] WIP

---
+++ kogito-codegen-modules/kogito-codegen-core/src/test/java/org/kie/kogito/codegen/core/CustomDashboardGeneratedUtilsTest.java
@@ -0,0 +1,11 @@
+package org.kie.kogito.codegen.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CustomDashboardGeneratedUtilsTest {
+
+    @Test
+    void loadCustomGrafanaDashboardsList() {
+    }
+}
