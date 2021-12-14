2e721822070db6ac4a6f9520003a5fa91f3415c7
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2021-12-14 16:26:45 +0100

    [KOGITO-5932] FORMAT

--- addons/common/monitoring/prometheus/src/main/java/org/kie/kogito/monitoring/prometheus/common/rest/MetricsResource.java
+++ addons/common/monitoring/prometheus/src/main/java/org/kie/kogito/monitoring/prometheus/common/rest/MetricsResource.java
@@ -18,7 +18,7 @@ package org.kie.kogito.monitoring.prometheus.common.rest;
 /**
  * This class must always have exact FQCN as <code>org.kie.kogito.monitoring.prometheus.common.rest.MetricsResource</code>
  *
- * @see  org.kie.kogito.codegen.api.utils.AddonsConfigDiscovery
+ * @see org.kie.kogito.codegen.api.utils.AddonsConfigDiscovery
  */
 public interface MetricsResource {
 
