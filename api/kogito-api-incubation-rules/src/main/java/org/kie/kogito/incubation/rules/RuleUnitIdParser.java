a9285eb6714fbb590a54e60243f2276859e54387
Edoardo Vacchi <evacchi@users.noreply.github.com>
2021-12-21 17:54:02 +0100

    ID parser

---
+++ api/kogito-api-incubation-rules/src/main/java/org/kie/kogito/incubation/rules/RuleUnitIdParser.java
@@ -0,0 +1,48 @@
+package org.kie.kogito.incubation.rules;
+
+import org.kie.kogito.incubation.common.LocalId;
+
+import java.util.StringTokenizer;
+
+public class RuleUnitIdParser {
+    public static LocalId parse(String id) {
+        return parse(id, LocalId.class);
+    }
+    public static <T extends LocalId> T parse(String id, Class<T> expected) {
+        StringTokenizer tok = new StringTokenizer(id, "/");
+        String prefix = tok.nextToken();
+
+        if (!prefix.equals(RuleUnitId.PREFIX)) throw newError(id, RuleUnitId.PREFIX);
+        String ruleUnit = tok.nextToken();
+        RuleUnitId ruleUnitId = new RuleUnitId(ruleUnit);
+        if (expected == RuleUnitId.class) return (T) ruleUnitId;
+        if (!tok.hasMoreTokens() && expected == LocalId.class) return (T) ruleUnitId;
+
+        String instanceOrQuery = tok.nextToken();
+        if (instanceOrQuery.equals(QueryId.PREFIX)) {
+            String query = tok.nextToken();
+            QueryId queryId = ruleUnitId.queries().get(query);
+            if (expected == QueryId.class) return (T) queryId;
+            if (!tok.hasMoreTokens() && expected == LocalId.class) return (T) queryId;
+        }
+        if (instanceOrQuery.equals(RuleUnitInstanceId.PREFIX)) {
+            String instanceId = tok.nextToken();
+            RuleUnitInstanceId ruleUnitInstanceId = ruleUnitId.instances().get(instanceId);
+            if (expected == RuleUnitInstanceId.class) return (T) ruleUnitInstanceId;
+            if (!tok.hasMoreTokens() && expected == LocalId.class) return (T) ruleUnitInstanceId;
+            if (!tok.nextToken().equals(QueryId.PREFIX)) throw newError(id, QueryId.PREFIX);
+            String query = tok.nextToken();
+            InstanceQueryId queryId = ruleUnitInstanceId.queries().get(query);
+            if (expected == InstanceQueryId.class) return (T) queryId;
+            if (!tok.hasMoreTokens() && expected == LocalId.class) return (T) queryId;
+        }
+        throw newError(id, "instances or queries");
+    }
+    public static <T extends LocalId> T extract(LocalId id, Class<T> expected) {
+        return parse(id.asLocalUri().path(), expected);
+    }
+
+    private static IllegalArgumentException newError(String id, String prefix) {
+        return new IllegalArgumentException("cannot parse " + id + ", expected " + prefix);
+    }
+}
---
+++ api/kogito-api-incubation-rules/src/test/java/org/kie/kogito/incubation/rules/RuleUnitIdParserTest.java
@@ -0,0 +1,40 @@
+package org.kie.kogito.incubation.rules;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleUnitIdParserTest {
+
+    @Test
+    void parseRuleUnitId() {
+        assertEquals(RuleUnitId.class, RuleUnitIdParser.parse("/rule-units/u").getClass());
+        assertEquals("u", RuleUnitIdParser.parse("/rule-units/u", RuleUnitId.class).ruleUnitId());
+    }
+
+    @Test
+    void parseQueryId() {
+        assertEquals(QueryId.class, RuleUnitIdParser.parse("/rule-units/u/queries/q").getClass());
+        assertEquals("u", RuleUnitIdParser.parse("/rule-units/u/queries/q", RuleUnitId.class).ruleUnitId());
+        assertEquals("q", RuleUnitIdParser.parse("/rule-units/u/queries/q", QueryId.class).queryId());
+    }
+
+
+    @Test
+    void parseRuleUnitInstanceId() {
+        assertEquals(RuleUnitInstanceId.class, RuleUnitIdParser.parse("/rule-units/u/instances/ui").getClass());
+        assertEquals("u", RuleUnitIdParser.parse("/rule-units/u/instances/ui", RuleUnitId.class).ruleUnitId());
+        assertEquals("ui", RuleUnitIdParser.parse("/rule-units/u/instances/ui", RuleUnitInstanceId.class).ruleUnitInstanceId());
+    }
+
+
+    @Test
+    void parseInstanceQueryId() {
+        assertEquals(InstanceQueryId.class, RuleUnitIdParser.parse("/rule-units/u/instances/ui/queries/q").getClass());
+        assertEquals("u", RuleUnitIdParser.parse("/rule-units/u/instances/ui/queries/q", RuleUnitId.class).ruleUnitId());
+        assertEquals("ui", RuleUnitIdParser.parse("/rule-units/u/instances/ui/queries/q", RuleUnitInstanceId.class).ruleUnitInstanceId());
+        assertEquals("q", RuleUnitIdParser.parse("/rule-units/u/instances/ui/queries/q", InstanceQueryId.class).queryId());
+    }
+
+
+}
