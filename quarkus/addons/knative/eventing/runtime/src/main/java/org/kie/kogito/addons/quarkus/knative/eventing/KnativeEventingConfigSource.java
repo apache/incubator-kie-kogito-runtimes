60831a1dbf257dfcf8ab81cdc222cda43d4d05dc
Ricardo Zanini <zanini@redhat.com>
2021-12-22 12:10:06 -0300

    include SPI module to kogito BOM
    Signed-off-by: Ricardo Zanini <zanini@redhat.com>

--- quarkus/addons/knative/eventing/runtime/src/main/java/org/kie/kogito/addons/quarkus/knative/eventing/KnativeEventingConfigSource.java
+++ quarkus/addons/knative/eventing/runtime/src/main/java/org/kie/kogito/addons/quarkus/knative/eventing/KnativeEventingConfigSource.java
@@ -41,7 +41,6 @@ public class KnativeEventingConfigSource implements ConfigSource {
     static {
         configuration.put("mp.messaging.outgoing." + KogitoEventStreams.OUTGOING + ".connector", "quarkus-http");
         configuration.put(URL_CONFIG, "${K_SINK}");
-        // TODO: disable protobuf: https://issues.redhat.com/browse/KOGITO-6453
     }
 
     /**
