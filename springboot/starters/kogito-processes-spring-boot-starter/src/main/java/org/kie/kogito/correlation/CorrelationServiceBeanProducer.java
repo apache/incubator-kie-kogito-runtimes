379c952f3aba4094d2b01e6d13924dc54aee622c
Tiago Dolphine <tiagodolphine@gmail.com>
2022-05-12 13:12:54 -0300

    Renaming CorrelationServiceProducer for spring

--- springboot/starters/kogito-processes-spring-boot-starter/src/main/java/org/kie/kogito/correlation/CorrelationServiceBeanProducer.java
+++ springboot/starters/kogito-processes-spring-boot-starter/src/main/java/org/kie/kogito/correlation/CorrelationServiceBeanProducer.java
@@ -20,7 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CorrelationServiceProducer {
+public class CorrelationServiceBeanProducer {
 
     @Bean
     CorrelationService correlationService() {
