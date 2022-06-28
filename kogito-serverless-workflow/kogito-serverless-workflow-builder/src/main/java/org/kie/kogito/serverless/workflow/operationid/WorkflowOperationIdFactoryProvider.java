db3dcd7fa855940b9c9395a8115bf4142e3abe1f
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2022-06-28 18:22:09 +0200

    [KOGITO-6687] Updating property name to include kogito

--- kogito-serverless-workflow/kogito-serverless-workflow-builder/src/main/java/org/kie/kogito/serverless/workflow/operationid/WorkflowOperationIdFactoryProvider.java
+++ kogito-serverless-workflow/kogito-serverless-workflow-builder/src/main/java/org/kie/kogito/serverless/workflow/operationid/WorkflowOperationIdFactoryProvider.java
@@ -23,7 +23,7 @@ import org.slf4j.LoggerFactory;
 public class WorkflowOperationIdFactoryProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(WorkflowOperationIdFactoryProvider.class);
-    public static final String PROPERTY_NAME = "quarkus.sw.operationIdStrategy";
+    public static final String PROPERTY_NAME = "quarkus.kogito.sw.operationIdStrategy";
 
     private static final WorkflowOperationIdFactoryType defaultType = WorkflowOperationIdFactoryType.FILE_NAME;
 
