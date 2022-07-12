54e3f451cfa01889c1802d4a80279906149f3400
Martin Weiler <mweiler@redhat.com>
2022-07-12 12:08:46 -0600

    KOGITO-7546: Improve regex handling in MessageContentExceptionPolicy

--- jbpm/jbpm-flow/src/main/java/org/jbpm/process/core/context/exception/MessageContentRegexExceptionPolicy.java
+++ jbpm/jbpm-flow/src/main/java/org/jbpm/process/core/context/exception/MessageContentRegexExceptionPolicy.java
@@ -15,12 +15,26 @@
  */
 package org.jbpm.process.core.context.exception;
 
-public class MessageContentExceptionPolicy extends AbstractHierarchyExceptionPolicy {
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MessageContentRegexExceptionPolicy extends AbstractHierarchyExceptionPolicy {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageContentRegexExceptionPolicy.class);
+
     @Override
     protected boolean verify(String errorCode, Throwable exception) {
         String msg = exception.getMessage();
         if (msg != null) {
-            return msg.toLowerCase().contains(errorCode.toLowerCase()) || msg.matches(errorCode);
+            try {
+                return Pattern.compile(errorCode, Pattern.CASE_INSENSITIVE).matcher(msg).find();
+            } catch (PatternSyntaxException ex) {
+                // will return false
+                LOGGER.debug("Failure parsing regular expression: {}", ex.getMessage());
+            }
         }
         return false;
     }
