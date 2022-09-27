20ec13aa657212f03e856b65d06d076b2791f4cb
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2022-09-27 13:56:32 +0200

    [KOGITO-7857] Adding annotations dependency on kogito-api
    This belongs to a different PR

--- api/kogito-api/src/main/java/org/kie/kogito/decision/DecisionModelMetadata.java
+++ api/kogito-api/src/main/java/org/kie/kogito/decision/DecisionModelMetadata.java
@@ -18,10 +18,12 @@ package org.kie.kogito.decision;
 import org.kie.kogito.ModelDomain;
 import org.kie.kogito.event.ModelMetadata;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public final class DecisionModelMetadata extends ModelMetadata {
 
+    @JsonProperty("specVersion")
     private String specVersion;
-    private ModelDomain modelDomain;
 
     public DecisionModelMetadata() {
         super(ModelDomain.DECISION);
@@ -34,11 +36,6 @@ public final class DecisionModelMetadata extends ModelMetadata {
 
     public String getSpecVersion() {
         return specVersion;
-    }
-
-    @Override
-    public ModelDomain getModelDomain() {
-        return modelDomain;
     }
 
 }
