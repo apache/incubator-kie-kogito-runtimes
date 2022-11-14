13d554e12b45276019c4c351ba0d475c1dbc7e4f
Paolo Bizzarri <pibizza@gmail.com>
2022-11-14 15:49:57 +0100

    KOGITO-8117] Missing cleanup

--- jbpm/jbpm-flow-builder/src/test/java/org/jbpm/integrationtests/ProcessTimerTest.java
+++ jbpm/jbpm-flow-builder/src/test/java/org/jbpm/integrationtests/ProcessTimerTest.java
@@ -33,8 +33,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 public class ProcessTimerTest extends AbstractBaseTest {
 
