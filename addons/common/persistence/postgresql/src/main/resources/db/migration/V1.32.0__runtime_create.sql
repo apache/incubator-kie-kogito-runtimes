ab7733070dcdb2011113a9393f15b37e3748ae6d
Abhishek Kumar <19465051+akumar074@users.noreply.github.com>
2022-12-06 12:22:20 +0530

    Incorporating PR comments

--- addons/common/persistence/postgresql/src/main/resources/db/migration/V1.32.0__runtime_create.sql
+++ addons/common/persistence/postgresql/src/main/resources/db/migration/V1.32.0__runtime_create.sql
@@ -1,3 +1,4 @@
+-- To be used with kogito-addons-quarkus-persistence-postgresql for Quarkus or kogito-addons-springboot-persistence-postgresql for SpringBoot
 CREATE TABLE process_instances
 (
     id              uuid              NOT NULL,
