e7b35ef35c98872c7a2a19a6a178647c46b52c00
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2023-03-14 16:46:30 +0100

    [KOGITO-8760] Adding unit test for REST

--- kogito-serverless-workflow/kogito-serverless-workflow-executor/src/test/java/org/kie/kogito/serverless/workflow/executor/StaticWorkflowApplicationTest.java
+++ kogito-serverless-workflow/kogito-serverless-workflow-executor/src/test/java/org/kie/kogito/serverless/workflow/executor/StaticWorkflowApplicationTest.java
@@ -19,15 +19,25 @@ import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
+import org.kie.kogito.jackson.utils.ObjectMapperFactory;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.TextNode;
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 import io.serverlessworkflow.api.Workflow;
+import io.serverlessworkflow.api.actions.Action;
 import io.serverlessworkflow.api.end.End;
+import io.serverlessworkflow.api.functions.FunctionDefinition;
+import io.serverlessworkflow.api.functions.FunctionRef;
 import io.serverlessworkflow.api.start.Start;
 import io.serverlessworkflow.api.states.DefaultState.Type;
 import io.serverlessworkflow.api.states.InjectState;
+import io.serverlessworkflow.api.states.OperationState;
+import io.serverlessworkflow.api.workflow.Functions;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StaticWorkflowApplicationTest {
@@ -41,6 +51,26 @@ class StaticWorkflowApplicationTest {
                     new InjectState(START_STATE, Type.INJECT).withData(new TextNode(GREETING_STRING)).withEnd(new End())))
                             .withStart(new Start().withStateName(START_STATE));
             assertThat(application.execute(workflow, Collections.emptyMap()).getWorkflowdata()).contains(new TextNode(GREETING_STRING));
+        }
+    }
+
+    @Test
+    void restInvocation() {
+        WireMockServer server = new WireMockServer();
+        JsonNode expectedOutput = ObjectMapperFactory.get().createObjectNode().put("name", "Javierito");
+        server.stubFor(get("/name").willReturn(aResponse().withStatus(200).withJsonBody(expectedOutput)));
+        final String START_STATE = "start";
+        final String FUNCTION_NAME = "function";
+        try (StaticWorkflowApplication application = StaticWorkflowApplication.create()) {
+            server.start();
+            Workflow workflow = new Workflow("HelloRest", "Hello Rest", "1.0", Arrays.asList(
+                    new OperationState().withName(START_STATE).withType(Type.OPERATION).withActions(Arrays.asList(new Action().withFunctionRef(new FunctionRef(FUNCTION_NAME)))).withEnd(new End())))
+                            .withStart(new Start().withStateName(START_STATE))
+                            .withFunctions(new Functions(Arrays.asList(new FunctionDefinition(FUNCTION_NAME).withOperation("rest:get:http://localhost:" + server.getOptions().portNumber() + "/name")
+                                    .withType(FunctionDefinition.Type.CUSTOM))));
+            assertThat(application.execute(workflow, Collections.emptyMap()).getWorkflowdata()).isEqualTo(expectedOutput);
+        } finally {
+            server.stop();
         }
     }
 }
