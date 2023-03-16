0b8e1ed6302898425c78f843b3f926dd91c4b4df
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2023-03-16 14:03:55 +0100

    [KOGITO-8855] Fluent API, first draft
    This only includes Operation and Inject. Working on Parallel and Switch,
    but I think this is enough to start commenting

---
+++ kogito-serverless-workflow/kogito-serverless-workflow-executor/src/main/java/org/kie/kogito/serverless/workflow/fluent/TransitionBuilder.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.serverless.workflow.fluent;
+
+import java.util.List;
+
+import io.serverlessworkflow.api.end.End;
+import io.serverlessworkflow.api.interfaces.State;
+
+public class TransitionBuilder<T> {
+
+    private final T container;
+    private List<State> states;
+
+    protected TransitionBuilder(T container, List<State> states) {
+        this.container = container;
+        this.states = states;
+    }
+
+    public TransitionBuilder<T> next(StateBuilder<?, ?> stateBuilder) {
+        states.add(stateBuilder.build());
+        return this;
+    }
+
+    public T end(StateBuilder<?, ?> stateBuilder) {
+        return end(stateBuilder, new End());
+    }
+
+    public T end(StateBuilder<?, ?> stateBuilder, End end) {
+        State state = stateBuilder.build(end);
+        states.add(state);
+        return container;
+    }
+}
---
+++ kogito-serverless-workflow/kogito-serverless-workflow-executor/src/test/java/org/kie/kogito/serverless/workflow/executor/StaticFluentWorkflowApplicationTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kie.kogito.serverless.workflow.executor;
+
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.kie.kogito.jackson.utils.ObjectMapperFactory;
+import org.kie.kogito.serverless.workflow.fluent.FunctionBuilder.HttpMethod;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import io.serverlessworkflow.api.Workflow;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.kie.kogito.serverless.workflow.fluent.ActionBuilder.functionCall;
+import static org.kie.kogito.serverless.workflow.fluent.FunctionBuilder.rest;
+import static org.kie.kogito.serverless.workflow.fluent.StateBuilder.inject;
+import static org.kie.kogito.serverless.workflow.fluent.StateBuilder.operation;
+import static org.kie.kogito.serverless.workflow.fluent.WorkflowBuilder.workflow;
+
+class StaticFluentWorkflowApplicationTest {
+
+    @Test
+    void helloWorld() {
+        final String GREETING_STRING = "Hello World!!!";
+        try (StaticWorkflowApplication application = StaticWorkflowApplication.create()) {
+            Workflow workflow = workflow("HelloWorld").singleton(inject(new TextNode(GREETING_STRING)));
+            assertThat(application.execute(workflow, Collections.emptyMap()).getWorkflowdata()).contains(new TextNode(GREETING_STRING));
+        }
+    }
+
+    @Test
+    void restInvocation() {
+        WireMockServer server = new WireMockServer();
+        JsonNode expectedOutput = ObjectMapperFactory.get().createObjectNode().put("name", "Javierito");
+        server.stubFor(get("/name").willReturn(aResponse().withStatus(200).withJsonBody(expectedOutput)));
+        final String FUNCTION_NAME = "function";
+        try (StaticWorkflowApplication application = StaticWorkflowApplication.create()) {
+            server.start();
+            Workflow workflow = workflow("HelloRest").function(rest(FUNCTION_NAME, HttpMethod.get, "http://localhost:" + server.getOptions().portNumber() + "/name"))
+                    .singleton(operation().action(functionCall(FUNCTION_NAME)));
+            assertThat(application.execute(workflow, Collections.emptyMap()).getWorkflowdata()).isEqualTo(expectedOutput);
+        } finally {
+            server.stop();
+        }
+    }
+}
