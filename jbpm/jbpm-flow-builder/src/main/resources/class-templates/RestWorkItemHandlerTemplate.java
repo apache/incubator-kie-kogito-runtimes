5c790fa652e52a62e5070213d3c22885a5c3fb1f
Francisco Javier Tirado Sarti <65240126+fjtirado@users.noreply.github.com>
2023-05-26 15:42:46 +0200

    [KOGITO-9220] Adding ssl client to RestWorkItemHandler (#3016)
    * [KOGITO-9220] Adding ssl client to RestWorkItemHandler
    
    * [KOGITO-9220] Reallocating produce to be available both for bmpn and swf

--- jbpm/jbpm-flow-builder/src/main/resources/class-templates/RestWorkItemHandlerTemplate.java
+++ jbpm/jbpm-flow-builder/src/main/resources/class-templates/RestWorkItemHandlerTemplate.java
@@ -16,18 +16,19 @@
 package org.jbpm.process.codegen;
 
 import io.vertx.mutiny.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.mutiny.core.Vertx;
 import org.kogito.workitem.rest.RestWorkItemHandler;
-import static org.kogito.workitem.rest.RestWorkItemHandlerUtils.vertx;
+import static org.kogito.workitem.rest.RestWorkItemHandlerUtils.sslWebClientOptions;
 
 public class xxxRestWorkItemHandler extends RestWorkItemHandler {
 
     public xxxRestWorkItemHandler() {
-        this(Vertx.vertx());
+        this(Vertx.vertx(), sslWebClientOptions());
     }
 
-    public xxxRestWorkItemHandler(Vertx vertx) {
-        super(WebClient.create(vertx(vertx)));
+    public xxxRestWorkItemHandler(Vertx vertx, WebClientOptions sslOptions) {
+    	super(WebClient.create(vertx), WebClient.create(vertx, sslOptions));
     }
     
     @Override
