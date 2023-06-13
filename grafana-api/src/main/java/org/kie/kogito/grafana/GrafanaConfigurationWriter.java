5b94e7aa6cfceec70b3271f669fe401287793405
Tibor Zimányi <tibor.zimanyi@ibm.com>
2023-06-13 17:03:01 +0200

    Fix Blocker and Critical Sonar reports

--- grafana-api/src/main/java/org/kie/kogito/grafana/GrafanaConfigurationWriter.java
+++ grafana-api/src/main/java/org/kie/kogito/grafana/GrafanaConfigurationWriter.java
@@ -20,11 +20,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -198,7 +199,11 @@ public class GrafanaConfigurationWriter {
 
     private static String customizeTemplate(String template, String handlerName, String artifactId, String version) {
         template = template.replaceAll("\\$handlerName\\$", handlerName);
-        template = template.replaceAll("\\$id\\$", String.valueOf(new Random().nextInt()));
+        try {
+            template = template.replaceAll("\\$id\\$", String.valueOf(SecureRandom.getInstanceStrong().nextInt()));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
         template = template.replaceAll("\\$uid\\$", UUID.randomUUID().toString());
         template = template.replaceAll("\\$gavArtifactId\\$", artifactId);
         template = template.replaceAll("\\$gavVersion\\$", version);
