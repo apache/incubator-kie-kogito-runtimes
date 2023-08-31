1e3357e6e3f7ce39eb4ead2010f9f9e726c5f060
Vani Haripriya Mudadla <vmudadla@redhat.com>
2023-08-31 12:39:37 -0500

    [KOGITO-9231] Updated Default bean

--- quarkus/extensions/kogito-quarkus-extension-common/kogito-quarkus-common/src/main/java/org/kie/kogito/quarkus/runtime/SSLWebClientOptionsProducer.java
+++ quarkus/extensions/kogito-quarkus-extension-common/kogito-quarkus-common/src/main/java/org/kie/kogito/quarkus/runtime/SSLWebClientOptionsProducer.java
@@ -18,25 +18,17 @@ package org.kie.kogito.quarkus.runtime;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Named;
 
 import io.quarkus.arc.DefaultBean;
 import io.vertx.ext.web.client.WebClientOptions;
 
 import static org.kie.kogito.quarkus.runtime.SSLWebClientOptionsUtils.sslQuarkusWebClientOptions;
-import static org.kogito.workitem.rest.RestWorkItemHandlerUtils.sslWebClientOptions;
 
 @ApplicationScoped
 public class SSLWebClientOptionsProducer {
 
     @Produces
     @DefaultBean
-    public WebClientOptions webClientOptions() {
-        return sslWebClientOptions();
-    }
-
-    @Produces
-    @Named("quarkusWebClientOptions")
     public WebClientOptions quarkusWebClientOptions() {
         return sslQuarkusWebClientOptions();
     }
