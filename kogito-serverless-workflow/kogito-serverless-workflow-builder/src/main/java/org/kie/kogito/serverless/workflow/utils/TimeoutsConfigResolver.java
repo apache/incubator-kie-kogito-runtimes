4a944605b0a4e9e3ffe6d3d0b00f8d437d146244
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2023-11-06 13:44:59 +0100

    [KOGITO-9811] Improving message to include the expression possibility

--- kogito-serverless-workflow/kogito-serverless-workflow-builder/src/main/java/org/kie/kogito/serverless/workflow/utils/TimeoutsConfigResolver.java
+++ kogito-serverless-workflow/kogito-serverless-workflow-builder/src/main/java/org/kie/kogito/serverless/workflow/utils/TimeoutsConfigResolver.java
@@ -29,9 +29,10 @@ import io.serverlessworkflow.api.timeouts.TimeoutsDefinition;
 
 public class TimeoutsConfigResolver {
 
-    private static final String NON_NEGATIVE_DURATION_MUST_BE_PROVIDED = "When configured, it must be set with a greater than zero ISO 8601 time duration. For example PT30S.";
+    private static final String NON_NEGATIVE_DURATION_MUST_BE_PROVIDED =
+            "When configured, it must be set with a greater than zero ISO 8601 time duration. For example PT30S. Or a valid expression, for example $CONST.myDuration, where 'myDuration' is defined in the constant section of the workflow";
 
-    private static final String INVALID_EVENT_TIMEOUT_FOR_STATE_ERROR = "An invalid \"eventTimeout\": \"%s\" configuration was provided for the state \"%s\" in the serverless workflow: \"%s\". " +
+    private static final String INVALID_EVENT_TIMEOUT_FOR_STATE_ERROR = "An invalid \"eventTimeout\": \"%s\" configuration was provided for the state \"%s\" in the serverless workflow: \"%s\"." +
             NON_NEGATIVE_DURATION_MUST_BE_PROVIDED;
 
     private static final String INVALID_EVENT_TIMEOUT_FOR_WORKFLOW_ERROR = "An invalid \"eventTimeout\": \"%s\" configuration was provided for the serverless workflow: \"%s\". " +
