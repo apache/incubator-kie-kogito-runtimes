8f731f14f8742ba501b51064d89262adb1b356cd
Enrique Gonzalez Martinez <Enrique.Gonzalez.Martinez1@ibm.com>
2024-05-16 16:18:50 +0200

    fix rest endpoints

--- kogito-codegen-modules/kogito-codegen-processes/src/main/java/org/kie/kogito/codegen/process/persistence/proto/AbstractProtoGenerator.java
+++ kogito-codegen-modules/kogito-codegen-processes/src/main/java/org/kie/kogito/codegen/process/persistence/proto/AbstractProtoGenerator.java
@@ -171,7 +171,7 @@ public abstract class AbstractProtoGenerator<T> implements ProtoGenerator {
         alreadyGenerated.addAll(protoBuiltins());
         for (T dataModel : dataModels.stream().filter(this::filterDataModels).toList()) {
             try {
-                LOGGER.info("internal proto geneartion {}", fqn(dataModel));
+                LOGGER.debug("internal proto geneartion {}", fqn(dataModel));
                 internalGenerate(
                         proto,
                         alreadyGenerated,
