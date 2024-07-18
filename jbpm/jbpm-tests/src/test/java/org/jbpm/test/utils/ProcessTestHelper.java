ee47ff3401b3f15ccea1e0aafcc96d23f8af98f7
Pere Fernández <pefernan@users.noreply.github.com>
2024-07-18 15:47:42 +0200

    - fix incubator apis to include SecurityPolicy in HT signals

--- jbpm/jbpm-tests/src/test/java/org/jbpm/test/utils/ProcessTestHelper.java
+++ jbpm/jbpm-tests/src/test/java/org/jbpm/test/utils/ProcessTestHelper.java
@@ -18,7 +18,9 @@
  */
 package org.jbpm.test.utils;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -68,21 +70,38 @@ public class ProcessTestHelper {
         ((DefaultWorkItemHandlerConfig) app.config().get(ProcessConfig.class).workItemHandlers()).register(handlerName, handler);
     }
 
+    /**
+     * Will be removed in favor of the {@link #completeWorkItem(ProcessInstance, Map, String, String...)}
+     */
+    @Deprecated
+    public static void completeWorkItem(ProcessInstance<? extends Model> processInstance, String userName, Map<String, Object> outputVars) {
+        completeWorkItem(processInstance, outputVars, item -> {
+        }, userName);
+    }
+
+    /**
+     * Will be removed in favor of the {@link #completeWorkItem(ProcessInstance, Map, Consumer, String, String...)}
+     */
+    @Deprecated
+    public static void completeWorkItem(ProcessInstance<? extends Model> processInstance, String userName, Map<String, Object> outputVars, Consumer<WorkItem> workItem) {
+        completeWorkItem(processInstance, outputVars, workItem, userName);
+    }
+
     public static void completeWorkItem(ProcessInstance<? extends Model> processInstance, Map<String, Object> outputVars) {
         completeWorkItem(processInstance, outputVars, item -> {
-        }, null, Collections.emptyList());
+        }, null);
     }
 
     public static void completeWorkItem(ProcessInstance<? extends Model> processInstance, Map<String, Object> outputVars, String userName, String... groups) {
         completeWorkItem(processInstance, outputVars, item -> {
-        }, userName, Arrays.asList(groups));
+        }, userName, groups);
     }
 
-    public static void completeWorkItem(ProcessInstance<? extends Model> processInstance, Map<String, Object> outputVars, Consumer<WorkItem> workItem, String userName, Collection<String> groups) {
-        List<WorkItem> workItems = processInstance.workItems(SecurityPolicy.of(userName, groups));
+    public static void completeWorkItem(ProcessInstance<? extends Model> processInstance, Map<String, Object> outputVars, Consumer<WorkItem> workItem, String userName, String... groups) {
+        List<WorkItem> workItems = processInstance.workItems(SecurityPolicy.of(userName, Arrays.asList(groups)));
         workItems.stream().findFirst().ifPresent(e -> {
             workItem.accept(e);
-            processInstance.completeWorkItem(e.getId(), outputVars, SecurityPolicy.of(userName, emptyList()));
+            processInstance.completeWorkItem(e.getId(), outputVars, SecurityPolicy.of(userName, Arrays.asList(groups)));
         });
     }
 
