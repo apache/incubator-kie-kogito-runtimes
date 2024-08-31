01b357cb6e64446384579798382e0a15a5c9eca5
Enrique Gonzalez Martinez <Enrique.Gonzalez.Martinez1@ibm.com>
2024-08-31 14:24:10 +0200

    fix python

--- kogito-serverless-workflow/kogito-serverless-workflow-python-runtime/src/main/java/org/kie/kogito/serverless/workflow/python/PythonScriptWorkItemHandler.java
+++ kogito-serverless-workflow/kogito-serverless-workflow-python-runtime/src/main/java/org/kie/kogito/serverless/workflow/python/PythonScriptWorkItemHandler.java
@@ -21,15 +21,15 @@ package org.kie.kogito.serverless.workflow.python;
 import java.util.Collections;
 import java.util.Map;
 
-import org.kie.kogito.internal.process.runtime.KogitoWorkItem;
+import org.kie.kogito.internal.process.workitem.KogitoWorkItem;
 import org.kie.kogito.serverless.workflow.WorkflowWorkItemHandler;
-
-import jep.Interpreter;
 
 import static org.kie.kogito.serverless.workflow.SWFConstants.PYTHON_SCRIPT;
 import static org.kie.kogito.serverless.workflow.SWFConstants.SCRIPT;
 import static org.kie.kogito.serverless.workflow.python.PythonWorkItemHandlerUtils.closeInterpreter;
 import static org.kie.kogito.serverless.workflow.python.PythonWorkItemHandlerUtils.interpreter;
+
+import jep.Interpreter;
 
 public class PythonScriptWorkItemHandler extends WorkflowWorkItemHandler {
 
