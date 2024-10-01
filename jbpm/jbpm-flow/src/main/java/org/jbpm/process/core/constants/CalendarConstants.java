b6984bf7014019d4729662b3fae8a6bbd4604d13
Abhiram Gundala <164050036+Abhitocode@users.noreply.github.com>
2024-10-01 09:03:26 -0400

    business-calendar

---
+++ jbpm/jbpm-flow/src/main/java/org/jbpm/process/core/constants/CalendarConstants.java
@@ -0,0 +1,6 @@
+package org.jbpm.process.core.constants;
+
+public class CalendarConstants {
+    public static final String BUSINESS_CALENDAR_PATH = "calendar.properties";
+    public static final String BUSINESS_CALENDAR_ENVIRONMENT_KEY = "jbpm.business.calendar";
+}
