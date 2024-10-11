95103f5067f79b3a454ae73e4b10ba024410e3d2
Enrique Gonzalez Martinez <Enrique.Gonzalez.Martinez1@ibm.com>
2024-10-11 16:51:43 +0200

    fix formatting

--- jbpm/jbpm-tests/src/test/java/org/jbpm/bpmn2/StandaloneBPMNProcessTest.java
+++ jbpm/jbpm-tests/src/test/java/org/jbpm/bpmn2/StandaloneBPMNProcessTest.java
@@ -28,7 +28,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 
