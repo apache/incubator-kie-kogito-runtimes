b5f7926d4a9d6c0fb9e1782f760853bafa92a583
Pere Fernández <pefernan@users.noreply.github.com>
2024-10-28 23:09:10 +0100

    [incubator-kie-issues-1597] Enforce authentication in User Tasks rest endpoints

--- api/kogito-api/src/main/java/org/kie/kogito/auth/IdentityProviderFactory.java
+++ api/kogito-api/src/main/java/org/kie/kogito/auth/IdentityProviderFactory.java
@@ -16,35 +16,15 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.kie.kogito.auth;
 
 import java.util.Collection;
 
 /**
- * Delivers security information about given identity that includes name and assigned roles.
- *
+ * Factory that resolves the {@link IdentityProvider}
  */
-public interface IdentityProvider {
+public interface IdentityProviderFactory {
 
-    /**
-     * Returns name assigned to the current context, usually refers to the username
-     * 
-     * @return assigned name taken from security context
-     */
-    String getName();
-
-    /**
-     * Returns roles assigned to the current context if any
-     * 
-     * @return Collection of assigned roles or empty one
-     */
-    Collection<String> getRoles();
-
-    /**
-     * Checks if given role is assigned to current context
-     * 
-     * @param role role to be checked
-     * @return true if the role is found otherwise null
-     */
-    boolean hasRole(String role);
+    IdentityProvider getOrImpersonateIdentity(String user, Collection<String> roles);
 }
