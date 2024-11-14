d852da7ac9f399558d510fe472ac07d17c679293
Francisco Javier Tirado Sarti <ftirados@redhat.com>
2024-11-14 17:05:51 +0100

    Try changing order of listeners

--- jbpm/jbpm-flow/src/main/java/org/kie/kogito/process/impl/AbstractProcessInstance.java
+++ jbpm/jbpm-flow/src/main/java/org/kie/kogito/process/impl/AbstractProcessInstance.java
@@ -85,7 +85,7 @@ public abstract class AbstractProcessInstance<T extends Model> implements Proces
     protected final T variables;
     protected final AbstractProcess<T> process;
     protected InternalProcessRuntime rt;
-    protected volatile WorkflowProcessInstance processInstance;
+    protected WorkflowProcessInstance processInstance;
 
     protected Integer status;
 
@@ -334,7 +334,7 @@ public abstract class AbstractProcessInstance<T extends Model> implements Proces
 
     @Override
     public int status() {
-        return processInstance != null ? processInstance.getState() : status;
+        return status;
     }
 
     @Override
