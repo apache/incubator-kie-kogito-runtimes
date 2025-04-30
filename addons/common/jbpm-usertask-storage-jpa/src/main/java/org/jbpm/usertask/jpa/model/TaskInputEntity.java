f142e20c20500c55dd526e027db8d9fbb3407220
Pere Fernández <pefernan@users.noreply.github.com>
2025-04-30 17:15:34 +0200

    - Fix it tests

--- addons/common/jbpm-usertask-storage-jpa/src/main/java/org/jbpm/usertask/jpa/model/TaskInputEntity.java
+++ addons/common/jbpm-usertask-storage-jpa/src/main/java/org/jbpm/usertask/jpa/model/TaskInputEntity.java
@@ -35,7 +35,8 @@ import jakarta.persistence.Table;
         @AttributeOverride(name = "value", column = @Column(name = "input_value"))
 })
 @AssociationOverride(name = "taskInstance",
-        joinColumns = @JoinColumn(name = "task_id", foreignKey = @ForeignKey(name = "fk_jbpm_user_tasks_inputs_tid")))
+        joinColumns = @JoinColumn(name = "task_id"),
+        foreignKey = @ForeignKey(name = "fk_jbpm_user_tasks_inputs_tid"))
 public class TaskInputEntity extends TaskNamedDataEntity<byte[]> {
 
 }
