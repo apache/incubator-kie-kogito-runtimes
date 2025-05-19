15031f8200ade96ac2c7850e25e6c2d75e0988d4
gabriel-farache <gfarache@redhat.com>
2025-05-19 17:31:37 +0200

    Add IT

--- quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/KeycloakServiceMock.java
+++ quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/KeycloakServiceMock.java
@@ -43,15 +43,24 @@ public class KeycloakServiceMock implements QuarkusTestResourceLifecycleManager 
 
     public static final String KEY_CLOAK_SERVICE_URL = "keycloak.mock.service.url";
     public static final String KEY_CLOAK_SERVICE_TOKEN_PATH = "keycloak.mock.service.token-path";
+
+    public static final String KEY_CLOAK_EXCHANGE_SERVICE_TOKEN_PATH = "keycloak.mock.exchange-service.token-path";
     public static final String REALM = "kogito-tests";
+    public static final String EXCHANGE_REALM = "kogito-exchange-tests";
     public static final String KEY_CLOAK_SERVICE_TOKEN_PATH_VALUE = "/realms/" + REALM + "/protocol/openid-connect/token";
+    public static final String KEY_CLOAK_EXCHANGE_SERVICE_TOKEN_PATH_VALUE = "/realms/" + EXCHANGE_REALM + "/protocol/openid-connect/token";
     public static final String CLIENT_ID = "kogito-app";
     public static final String SECRET = "secret";
     public static final String KEYCLOAK_ACCESS_TOKEN = "KEYCLOAK_ACCESS_TOKEN";
+
+    public static final String KEYCLOAK_EXCHANGED_ACCESS_TOKEN = "KEYCLOAK_EXCHANGED_ACCESS_TOKEN";
+
     public static final String KEYCLOAK_REFRESH_TOKEN = "KEYCLOAK_REFRESH_TOKEN";
     public static final String KEYCLOAK_SESSION_STATE = "KEYCLOAK_SESSION_STATE";
 
     public static final String AUTH_REQUEST_BODY = "grant_type=client_credentials";
+
+    public static final String EXCHANGE_AUTH_REQUEST_BODY = "grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Atoken-exchange";
 
     private WireMockServer wireMockServer;
 
@@ -69,23 +78,49 @@ public class KeycloakServiceMock implements QuarkusTestResourceLifecycleManager 
                         .withHeader(CONTENT_TYPE, APPLICATION_JSON)
                         .withBody(getTokenResult())));
 
+        stubFor(post(KEY_CLOAK_EXCHANGE_SERVICE_TOKEN_PATH_VALUE)
+                .withHeader(CONTENT_TYPE, equalTo(APPLICATION_FORM_URLENCODED))
+                .withBasicAuth(CLIENT_ID, SECRET)
+                .withRequestBody(equalTo(EXCHANGE_AUTH_REQUEST_BODY))
+                .willReturn(aResponse()
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON)
+                        .withBody(exchangeTokenResult())));
+
         Map<String, String> properties = new HashMap<>();
         properties.put(KEY_CLOAK_SERVICE_URL, wireMockServer.baseUrl());
         properties.put(KEY_CLOAK_SERVICE_TOKEN_PATH, KEY_CLOAK_SERVICE_TOKEN_PATH_VALUE);
+        properties.put(KEY_CLOAK_EXCHANGE_SERVICE_TOKEN_PATH, KEY_CLOAK_EXCHANGE_SERVICE_TOKEN_PATH_VALUE);
         return properties;
     }
 
     private static String getTokenResult() {
-        return "{\n" +
-                "    \"access_token\": \"" + KEYCLOAK_ACCESS_TOKEN + "\",\n" +
-                "    \"expires_in\": 300,\n" +
-                "    \"refresh_expires_in\": 1800,\n" +
-                "    \"refresh_token\": \"" + KEYCLOAK_REFRESH_TOKEN + "\",\n" +
-                "    \"token_type\": \"bearer\",\n" +
-                "    \"not-before-policy\": 0,\n" +
-                "    \"session_state\": \"" + KEYCLOAK_SESSION_STATE + "\",\n" +
-                "    \"scope\": \"email profile\"\n" +
-                "}";
+        return """
+                {
+                    "access_token": "%s",
+                    "expires_in": 300,
+                    "refresh_expires_in": 1800,
+                    "refresh_token": "%s",
+                    "token_type": "bearer",
+                    "not-before-policy": 0,
+                    "session_state": "%s",
+                    "scope": "email profile"
+                }
+                """.formatted(KEYCLOAK_ACCESS_TOKEN, KEYCLOAK_REFRESH_TOKEN, KEYCLOAK_SESSION_STATE);
+    }
+
+    private static String exchangeTokenResult() {
+        return """
+                {
+                    "access_token": "%s",
+                    "expires_in": 300,
+                    "refresh_expires_in": 1800,
+                    "refresh_token": "%s",
+                    "token_type": "bearer",
+                    "not-before-policy": 0,
+                    "session_state": "%s",
+                    "scope": "email profile"
+                }
+                """.formatted(KEYCLOAK_EXCHANGED_ACCESS_TOKEN, KEYCLOAK_REFRESH_TOKEN, KEYCLOAK_SESSION_STATE);
     }
 
     @Override
