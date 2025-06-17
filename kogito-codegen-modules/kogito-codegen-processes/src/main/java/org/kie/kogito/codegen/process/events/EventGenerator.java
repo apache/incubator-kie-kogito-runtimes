63ce2c90219d03088fe6f93c0089dbafa4e6a4eb
Enrique Gonzalez Martinez <Enrique.Gonzalez.Martinez1@ibm.com>
2025-06-17 11:38:26 +0200

    fix naming in codegen

--- kogito-codegen-modules/kogito-codegen-processes/src/main/java/org/kie/kogito/codegen/process/events/EventGenerator.java
+++ kogito-codegen-modules/kogito-codegen-processes/src/main/java/org/kie/kogito/codegen/process/events/EventGenerator.java
@@ -124,7 +124,20 @@ public abstract class EventGenerator implements ClassGenerator {
     }
 
     private static String polishClassName(ChannelInfo channelInfo, String suffix) {
-        return channelInfo.getChannelName().replaceAll("[\\s-]", "") + suffix;
+        String channelName = channelInfo.getChannelName();
+        String[] words = channelName.split("[\\W_]+");
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (i == 0) {
+                word = word.isEmpty() ? word : word.toLowerCase();
+            } else {
+                word = word.isEmpty() ? word : Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
+            }
+            builder.append(word);
+        }
+        return builder.toString() + suffix;
     }
 
     @Override
