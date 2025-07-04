ae8eb7d8daad75220095b5d102c06b43db81fce0
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2025-07-04 16:27:07 +0200

    [incubator-kie-issues#2005] Fix - verify ConfigProvider availability

--- kogito-codegen-modules/kogito-codegen-decisions/src/main/java/org/kie/kogito/codegen/decision/DecisionCodegen.java
+++ kogito-codegen-modules/kogito-codegen-decisions/src/main/java/org/kie/kogito/codegen/decision/DecisionCodegen.java
@@ -30,6 +30,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.drools.codegen.common.GeneratedFile;
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.config.ConfigValue;
 import org.eclipse.microprofile.openapi.spi.OASFactoryResolver;
@@ -165,10 +166,19 @@ public class DecisionCodegen extends AbstractGenerator {
     }
 
     boolean getEnableRuntimeTypeCheckOption() {
-        ConfigValue configValue = ConfigProvider.getConfig().getConfigValue(RuntimeTypeCheckOption.PROPERTY_NAME);
+        Config config = null;
+        try {
+            config = ConfigProvider.getConfig();
+        } catch (IllegalStateException e) {
+            LOGGER.warn("Failed to instantiate a Config from ConfigProvider, reading {} from Properties", RuntimeTypeCheckOption.PROPERTY_NAME);
+        }
+        return config != null ? getEnableRuntimeTypeCheckOptionFromConfig(config) : getEnableRuntimeTypeCheckOptionFromProperties();
+    }
+
+    boolean getEnableRuntimeTypeCheckOptionFromConfig(Config config) {
+        ConfigValue configValue = config.getConfigValue(RuntimeTypeCheckOption.PROPERTY_NAME);
         return configValue != null && configValue.getValue() != null && !configValue.getValue().isEmpty() ? Boolean.parseBoolean(configValue.getValue())
                 : getEnableRuntimeTypeCheckOptionFromProperties();
-
     }
 
     boolean getEnableRuntimeTypeCheckOptionFromProperties() {
