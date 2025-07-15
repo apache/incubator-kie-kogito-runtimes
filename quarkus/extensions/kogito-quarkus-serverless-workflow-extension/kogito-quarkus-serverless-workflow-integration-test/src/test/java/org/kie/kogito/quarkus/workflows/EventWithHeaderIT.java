1669d838a431ad63995b87817f4b1ff9cb9e4403
gabriel-farache <gfarache@redhat.com>
2025-07-15 15:39:56 +0200

    Apply feedback
    Signed-off-by: gabriel-farache <gfarache@redhat.com>

--- quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/EventWithHeaderIT.java
+++ quarkus/extensions/kogito-quarkus-serverless-workflow-extension/kogito-quarkus-serverless-workflow-integration-test/src/test/java/org/kie/kogito/quarkus/workflows/EventWithHeaderIT.java
@@ -18,8 +18,10 @@
  */
 package org.kie.kogito.quarkus.workflows;
 
+import java.io.IOException;
 import java.net.URI;
 import java.time.OffsetDateTime;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -28,6 +30,8 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.kie.kogito.event.Converter;
+import org.kie.kogito.event.impl.StringCloudEventUnmarshallerFactory;
 import org.kie.kogito.test.quarkus.QuarkusTestProperty;
 import org.kie.kogito.test.quarkus.kafka.KafkaTestClient;
 import org.kie.kogito.testcontainers.quarkus.KafkaQuarkusTestResource;
@@ -37,6 +41,7 @@ import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import io.cloudevents.CloudEvent;
 import io.cloudevents.core.builder.CloudEventBuilder;
 import io.cloudevents.jackson.JsonCloudEventData;
 import io.cloudevents.jackson.JsonFormat;
@@ -56,6 +61,8 @@ class EventWithHeaderIT {
     private static final String SERVICE_GET_BY_ID_URL = SERVICE_URL + "/{id}";
     private static final String EVENT_TYPE = "lock-event";
     private static final String EVENT_TOPIC = "lock-event";
+
+    private static final String EXPECTED_CALLBACK_EVENT_TYPE = "id-event";
 
     private static final String DEFAULT_OUT_EVENT_TOPIC = "kogito-sw-out-events";
 
@@ -86,9 +93,9 @@ class EventWithHeaderIT {
 
     @Test
     @SuppressWarnings("squid:S2699")
-    void callbackStateSuccessful() throws Exception {
+    void triggerWorkflowWithCloudEvent() throws Exception {
 
-        String response = objectMapper.writeValueAsString(CloudEventBuilder.v1()
+        String lockEvent = objectMapper.writeValueAsString(CloudEventBuilder.v1()
                 .withId(UUID.randomUUID().toString())
                 .withSource(URI.create(""))
                 .withType(EventWithHeaderIT.EVENT_TYPE)
@@ -98,15 +105,24 @@ class EventWithHeaderIT {
                 .build());
 
         final CountDownLatch countDownLatch = new CountDownLatch(1);
-        final AtomicReference<String> cloudEvent = new AtomicReference<>();
-        kafkaClient.consume(DEFAULT_OUT_EVENT_TOPIC, rawCloudEvent -> {
-            cloudEvent.set(rawCloudEvent);
-            countDownLatch.countDown();
+        final Converter<String, CloudEvent> converter = new StringCloudEventUnmarshallerFactory(objectMapper).unmarshaller(Map.class).cloudEvent();
+        final AtomicReference<String> idEvent = new AtomicReference<>();
+        kafkaClient.consume(DEFAULT_OUT_EVENT_TOPIC, rawIdEventCloudEvent -> {
+            idEvent.set(rawIdEventCloudEvent);
+            try {
+                CloudEvent event = converter.convert(rawIdEventCloudEvent);
+                if (EXPECTED_CALLBACK_EVENT_TYPE.equals(event.getType()) &&
+                        "event-with-headers".equals(event.getExtension("kogitoprocid"))) {
+                    countDownLatch.countDown();
+                }
+            } catch (IOException e) {
+                LOGGER.error("Error while converting raw CloudEvent to CloudEvent map: {}", rawIdEventCloudEvent, e);
+            }
         });
-        kafkaClient.produce(response, EventWithHeaderIT.EVENT_TOPIC);
-        // give some time to consume the event and verify the expected decision was made.
+        kafkaClient.produce(lockEvent, EventWithHeaderIT.EVENT_TOPIC);
+        // give some time to consume the event and produce the output idEvent CloudEvent
         assertThat(countDownLatch.await(15, TimeUnit.SECONDS)).isTrue();
-        String processInstanceId = new JsonPath(cloudEvent.get()).get("data");
+        String processInstanceId = new JsonPath(idEvent.get()).get("data");
         LOGGER.info("processInstanceId: {}", processInstanceId);
 
         // give some time for the event to be processed and the process to finish.
