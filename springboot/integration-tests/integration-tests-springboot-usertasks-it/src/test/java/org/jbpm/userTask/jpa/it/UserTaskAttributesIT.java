15e69809043ce0129daeae0a8ca941cb4be682a4
Martin Cimbalek <martin.cimbalek@ibm.com>
2025-10-15 11:35:12 +0200

    [kie-isses-2088] add license headers and fix tests not actually being parametrized

--- springboot/integration-tests/integration-tests-springboot-usertasks-it/src/test/java/org/jbpm/userTask/jpa/it/UserTaskAttributesIT.java
+++ springboot/integration-tests/integration-tests-springboot-usertasks-it/src/test/java/org/jbpm/userTask/jpa/it/UserTaskAttributesIT.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.jbpm.userTask.jpa.it;
 
 import java.util.Set;
@@ -63,20 +82,20 @@ public class UserTaskAttributesIT extends BaseUserTaskIT {
                 .extract()
                 .path("[0].id");
 
-        addAndVerify(ADMIN_USERS_RESOURCE, taskId, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2"));
-        ValidatableResponse response = addAndVerify(ADMIN_USERS_RESOURCE, taskId, Set.of(input.toLowerCase() + "2", input + "4"));
-        verifyResponseResourceContainsExactly(response, ADMIN_USERS_RESOURCE, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2", input + "4"));
+        addAndVerify(input, taskId, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2"));
+        ValidatableResponse response = addAndVerify(input, taskId, Set.of(input.toLowerCase() + "2", input + "4"));
+        verifyResponseResourceContainsExactly(response, input, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2", input + "4"));
 
-        setAndVerify(ADMIN_USERS_RESOURCE, taskId, Set.of("Replacement" + input + "1", "Replacement" + input + "2"));
-        setAndVerify(ADMIN_USERS_RESOURCE, taskId, Set.of("Replacement" + input + "2", input + "3"));
+        setAndVerify(input, taskId, Set.of("Replacement" + input + "1", "Replacement" + input + "2"));
+        setAndVerify(input, taskId, Set.of("Replacement" + input + "2", input + "3"));
 
-        addAndVerify(ADMIN_USERS_RESOURCE, taskId, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2"));
+        addAndVerify(input, taskId, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2"));
 
-        response = removeAndVerify(ADMIN_USERS_RESOURCE, taskId, Set.of(input.toUpperCase() + "1"));
-        verifyResponseResourceContainsExactly(response, ADMIN_USERS_RESOURCE, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2", "Replacement" + input + "2", input + "3"));
+        response = removeAndVerify(input, taskId, Set.of(input.toUpperCase() + "1"));
+        verifyResponseResourceContainsExactly(response, input, Set.of(input.toLowerCase() + "1", input.toLowerCase() + "2", "Replacement" + input + "2", input + "3"));
 
-        response = removeAndVerify(ADMIN_USERS_RESOURCE, taskId, Set.of(input.toLowerCase() + "1", "Replacement" + input + "2"));
-        verifyResponseResourceContainsExactly(response, ADMIN_USERS_RESOURCE, Set.of(input.toLowerCase() + "2", input + "3"));
+        response = removeAndVerify(input, taskId, Set.of(input.toLowerCase() + "1", "Replacement" + input + "2"));
+        verifyResponseResourceContainsExactly(response, input, Set.of(input.toLowerCase() + "2", input + "3"));
     }
 
     private ValidatableResponse addAndVerify(String resource, String taskId, Set<String> entries) {
