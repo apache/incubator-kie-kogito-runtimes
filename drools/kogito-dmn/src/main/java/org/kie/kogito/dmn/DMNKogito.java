99b3c473ef1eea313289814e366f5f0e521d6b94
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2026-05-04 15:58:01 +0200

    [incubator-kie-issues#2253] Fix impsort

--- drools/kogito-dmn/src/main/java/org/kie/kogito/dmn/DMNKogito.java
+++ drools/kogito-dmn/src/main/java/org/kie/kogito/dmn/DMNKogito.java
@@ -22,7 +22,6 @@ import java.io.InputStream;
 import java.io.Reader;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
